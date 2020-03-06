27166ca0df257c33be951a07ba9bd901fba69fe1
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2020-03-06 16:52:33 +0100

    ADD: spack version option

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+version="v0.14.0"
 
 TEMP=$@
 eval set -- "$TEMP --"
@@ -6,6 +7,7 @@ while true; do
     case "$1" in
         --idir|-i) install_dir=$2; shift 2;;
         --machine|-m) hostname=$2; shift 2;;
+        --version|-v) version=$2; shift 2;;
         --help|-h) help_enabled=yes; fwd_args="$fwd_args $1"; shift;;
         -- ) shift; break ;;
         * ) fwd_args="$fwd_args $1"; shift ;;
@@ -16,7 +18,8 @@ if [[ "${help_enabled}" == "yes" ]]; then
     echo "Available Options:"
     echo "* --help.  |-h {print help}"
     echo "* --machine|-m {machine name}     Required"
-    echo "* --idir.  |-d {package dir}      Where the Spack instance will be installed. Default: \$(pwd)"
+    echo "* --version|-v {spack version}     Default: v0.14.0"
+    echo "* --idir.  |-i {install dir}      Where the Spack instance will be installed. Default: \$(pwd)"
 fi
 
 if [[ -z ${install_dir} ]]; then
@@ -25,7 +28,7 @@ fi
 
 echo "Cloning spack instance to:" $install_dir
 
-git clone git@github.com:spack/spack.git $install_dir/spack
+git clone git@github.com:spack/spack.git -b $version $install_dir/spack
 
 echo "Installing mch packages &" $hostname "config files"
 
