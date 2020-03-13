49f40cf09747b25547693376476de557bd678e6e
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2020-03-13 09:20:43 +0100

    FIX: config.sh for developement

--- config.sh
+++ config.sh
@@ -8,6 +8,7 @@ while true; do
         --idir|-i) install_dir=$2; shift 2;;
         --machine|-m) hostname=$2; shift 2;;
         --version|-v) version=$2; shift 2;;
+        --reposdir|-r) reposdir=$2; shift 2;;
         --help|-h) help_enabled=yes; fwd_args="$fwd_args $1"; shift;;
         -- ) shift; break ;;
         * ) fwd_args="$fwd_args $1"; shift ;;
@@ -19,24 +20,26 @@ if [[ "${help_enabled}" == "yes" ]]; then
     echo "* --help.  |-h {print help}"
     echo "* --machine|-m {machine name}     Required"
     echo "* --version|-v {spack version}     Default: v0.14.0"
-    echo "* --idir.  |-i {install dir}      Where the Spack instance will be installed. Default: \$(pwd)"
+    echo "* --idir.  |-i {install dir}      Where the Spack instance is installed or you want it to be installed. Default: \$(pwd)"
 fi
 
 if [[ -z ${install_dir} ]]; then
   install_dir=$PWD
 fi
 
-echo "Cloning spack instance to:" $install_dir
-
-git clone git@github.com:spack/spack.git -b $version $install_dir/spack
+if [[ -z ${install_dir}/spack ]]; then
+    echo "Cloning spack instance to:" $install_dir
+    git clone git@github.com:spack/spack.git -b $version $install_dir/spack
+fi
 
 echo "Installing mch packages &" $hostname "config files"
 
-cp -rf $PWD/packages/* $install_dir/spack/var/spack/repos/builtin/packages
+if [[ -n ${reposdir} ]]; then
+    echo " -$PWD" >> repos.yaml
+    cp repos.yaml $reposdir/
+fi
 
 cp -rf $PWD/sysconfigs/$hostname/* $install_dir/spack/etc/spack
-
-cp -rf $PWD/repos.yaml $install_dir/spack/etc/spack
 
 echo "MCH Spack installed"
 
