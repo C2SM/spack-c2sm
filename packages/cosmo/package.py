b093ee89b3b144163dbba2061d92e4b2367b1f62
cosunae <carlos.osuna@meteoswiss.ch>
2020-04-05 23:18:35 +0200

    depend on omni only when using claw

--- packages/cosmo/package.py
+++ packages/cosmo/package.py
@@ -42,7 +42,7 @@ class Cosmo(MakefilePackage):
     depends_on('cosmo-grib-api-definitions', when='~eccodes')
     depends_on('cosmo-eccodes-definitions@2.14.1.2', when='+eccodes')
     depends_on('perl@5.16.3:')
-    depends_on('omni-xmod-pool')
+    depends_on('omni-xmod-pool', when='+claw')
     depends_on('claw', when='+claw')
     depends_on('boost', when='cosmo_target=gpu ~cppdycore')
 
