cd16f4b14c159264cb6a22e00abf1b44d68e98e8
cosunae <carlos.osuna@env.ethz.ch>
2020-05-01 10:13:00 +0200

    add new packages

---
+++ packages/atlas/package.py
@@ -0,0 +1,33 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+#
+from spack import *
+
+
+class Atlas(CMakePackage):
+    """A library for numerical weather prediction and climate modelling"""
+
+    homepage = 'https://confluence.ecmwf.int/display/atlas'
+    url      = "https://github.com/ecmwf/atlas/archive/0.20.2.tar.gz"
+    git      = 'https://github.com/ecmwf/atlas.git'
+    maintainers = ['cosunae']
+
+    version('master', branch='master')
+
+    depends_on('ecbuild')
+    depends_on('eckit')
+
+    def cmake_args(self):
+        args = []
+        spec = self.spec
+
+        args.append('-DCMAKE_MODULE_PATH={0}/share/ecbuild/cmake'.
+                    format(spec['ecbuild'].prefix))
+        args.append('-Deckit_DIR={0}'.format(spec['eckit'].prefix))
+        args.append('-DENABLE_GRIDTOOLS_STORAGE=OFF')
+
+        return args
+
---
+++ packages/atlas_utilities/package.py
@@ -0,0 +1,38 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+#
+from spack import *
+
+
+class AtlasUtilities(CMakePackage):
+    """This repoistory contains some utilities to transform Atlas meshes as well as the first ICON stencil prototype computing a Laplacian."""
+
+    homepage = 'https://github.com/cosunae/AtlasUtilities'
+    url      = "https://github.com/cosunae/AtlasUtilities"
+    git      = 'https://github.com/cosunae/AtlasUtilities'
+    maintainers = ['cosunae']
+
+    version('master', branch='cmake_netcdf')
+
+    depends_on('atlas')
+    depends_on('eckit')
+    depends_on('netcdf-cxx4')
+    depends_on('netcdf-c')
+
+    def cmake_args(self):
+        args = []
+        spec = self.spec
+
+        args.append('-DCMAKE_MODULE_PATH={0}/share/ecbuild/cmake'.
+                    format(spec['ecbuild'].prefix))
+        args.append('-Deckit_DIR={0}'.format(spec['eckit'].prefix))
+        args.append('-Datlas_DIR={0}'.format(spec['atlas'].prefix))
+        args.append('-Dnetcdfcxx4_DIR='.format(self.spec['netcdf-cxx4'].prefix))
+        args.append('-Dnetcdf_DIR='.format(spec['netcdf-c'].prefix))
+
+
+        return args
+
