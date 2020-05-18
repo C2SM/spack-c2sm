2b5d87332d41348d033447a125ce2b88532445ca
cosunae <carlos.osuna@meteoswiss.ch>
2020-05-19 01:07:06 +0200

    add dawn

---
+++ packages/dawn4py/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+#
+from spack import *
+import sys
+
+class Dawn4py(PythonPackage):
+    """A library for numerical weather prediction and climate modelling"""
+
+    homepage = 'https://github.com/MeteoSwiss-APN/dawn'
+    url      = "https://github.com/MeteoSwiss-APN/dawn/archive/0.0.1.tar.gz"
+    git      = 'https://github.com/MeteoSwiss-APN/dawn'
+    maintainers = ['cosunae']
+
+    version('master', branch='master')
+
+    depends_on('cmake')
+    depends_on('python@3.8.0', type=('build','run'))
+
+    build_directory = 'dawn'
+    phases = ['build', 'install']
+
