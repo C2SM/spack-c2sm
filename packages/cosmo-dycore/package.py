51877be240b498e59e8aac7259facfb42d189ffd
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2020-06-04 15:31:26 +0200

    ADD: reservation for rh7.7 regression tests

--- packages/cosmo-dycore/package.py
+++ packages/cosmo-dycore/package.py
@@ -128,7 +128,7 @@ class CosmoDycore(CMakePackage):
                     run_unittests = Executable('srun --time=00:05:00 -C gpu -p normal -A g110 -N 1 ./unittests  --gtest_filter=-TracerBindings.TracerVariable')
                 run_unittests()
             with working_dir(prefix + '/tests/unittests/gcl_fortran'):
-                if self.spec.variants['slave'].value == 'tsa':
+                if 'tsa' in self.spec.variants['slave'].value:
                     if self.spec.variants['slave'].value == 'tsa_rh7.7':
                         run_unitests_gcl_bindings = Executable('srun --reservation=rh77 -n 4 -p normal --gres=gpu:4 ./unittests_gcl_bindings')
                     else:
@@ -140,7 +140,10 @@ class CosmoDycore(CMakePackage):
                 testlist=['cosmo1_cp_test1', 'cosmo-1e_test_1', 'cosmo-1e_test_1_all_off', 'cosmo-1e_test_1_coldpool_uv', 'cosmo-1e_test_1_non_default', 'cosmo-1e_test_1_vdiffm1', 'cosmo7_test_3', 'cosmo7_test_namelist_irunge_kutta2', 'cosmo-2e_test_1', 'cosmo-2e_test_1_coldpools', 'cosmo-2e_test_1_bechtold']
                 for test in testlist:
                     if 'tsa' in self.spec.variants['slave'].value:
-                        run_regression_test = Executable('srun -n 1 -p debug --gres=gpu:1 ./regression_tests -p ' + self.spec.variants['data_path'].value + self.spec.variants['real_type'].value + '/' + test + ' --gtest_filter=-DycoreUnittest.Performance')
+                        if self.spec.variants['slave'].value == 'tsa_rh7.7':
+                            run_regression_test = Executable('srun --reservation=rh77 -n 1 -p debug --gres=gpu:1 ./regression_tests -p ' + self.spec.variants['data_path'].value + self.spec.variants['real_type'].value + '/' + test + ' --gtest_filter=-DycoreUnittest.Performance')
+                        else:
+                            run_regression_test = Executable('srun -n 1 -p debug --gres=gpu:1 ./regression_tests -p ' + self.spec.variants['data_path'].value + self.spec.variants['real_type'].value + '/' + test + ' --gtest_filter=-DycoreUnittest.Performance')
                     if self.spec.variants['slave'].value == 'daint':
                         run_regression_test = Executable('srun --time=00:05:00 -C gpu -p normal -A g110 -N 1 ./regression_tests -p ' + self.spec.variants['data_path'].value + self.spec.variants['real_type'].value + '/' + test + ' --gtest_filter=-DycoreUnittest.Performance')
                     run_regression_test()
