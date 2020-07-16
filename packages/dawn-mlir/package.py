9d2b92dbbe7a87d44883460dab7a5d58d7f060b0
cosunae <carlos.osuna@env.ethz.ch>
2020-07-16 15:11:57 +0200

    add dawn mlir

--- packages/dawn-mlir/package.py
+++ packages/dawn-mlir/package.py
@@ -25,32 +25,24 @@
 from spack import *
 import subprocess, re
 
-class Mlir(CMakePackage):
-    """Build for MLIR with llvm"""
+class DawnMlir(CMakePackage):
+    """Build for dawn with mlir"""
     
-    homepage = "https://github.com/llvm/llvm-project.git"
-    git      = "git@github.com:llvm/llvm-project.git"
+    homepage = "https://github.com/dawn-ico/dawn-mlir.git"
+    git      = "git@github.com:dawn-ico/dawn-mlir.git"
     maintainers = ['cosunae']
     
     version('master', branch='master')
-    version('dawn', commit='10643c9ad85')
 
     depends_on('cmake@3.12:%gcc', type='build')
     depends_on('cuda', when='+cuda', type=('build', 'run'))
+    depends_on('mlir@dawn')
 
-    root_cmakelists_dir='llvm'
-    
     def cmake_args(self):
       spec = self.spec
 
       args = []
-      args.append("-DLLVM_BUILD_EXAMPLES=OFF")
-      args.append("-DLLVM_TARGETS_TO_BUILD=host;NVPTX;AMDGPU")
-      args.append("-DLLVM_ENABLE_PROJECTS=mlir;lld")
-      args.append("-DLLVM_OPTIMIZED_TABLEGEN=ON")
-      args.append("-DLLVM_ENABLE_OCAMLDOC=OFF")
-      args.append("-DLLVM_ENABLE_BINDINGS=OFF")
-      args.append("-DLLVM_INSTALL_UTILS=ON")
-      #args.append("-DMLIR_CUDA_RUNNER_ENABLED=ON")
+      args.append('-DMLIR_DIR={0}'.format(spec['mlir'].prefix+'/lib/cmake/mlir'))
+      #-DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit
       
       return args
