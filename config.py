a7e3ad2ad913471b831c2212504705533004530f
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2020-08-06 09:40:17 +0200

    FIX: spack branch to newest version

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import yaml
 import shutil
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-spack_version='v0.15.0'
+spack_version='releases/latest'
 
 def main():
     parser=argparse.ArgumentParser(description='Small config script which can be used to install a spack instance with the correct configuration files and mch spack packages.')
