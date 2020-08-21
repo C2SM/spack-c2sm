d1cd7da2acf523c6da8822d661e1fe6e310e5b52
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2020-08-21 17:58:28 +0200

    FIX: dycore spec extraction even more general

--- tools/spack-scripting/scripting/cmd/devbuildcosmo.py
+++ tools/spack-scripting/scripting/cmd/devbuildcosmo.py
@@ -56,10 +56,10 @@ def devbuildcosmo(self, args):
     dycore_spec = 'cosmo-dycore@dev-build'
 
     # Extracting dycore variants
-    dycore_spec += cosmo_spec.format('{^cosmo-dycore.variants}')
+    dycore_spec = cosmo_spec.format('{^cosmo-dycore.name}') + cosmo_spec.format('{^cosmo-dycore.@version}') + cosmo_spec.format('{^cosmo-dycore.%compiler}') + cosmo_spec.format('{^cosmo-dycore.variants}')
 
     # Extracting correct mpi variant
-    dycore_spec += ' ^' + cosmo_spec.format('{^mpi.name}') + '%' + cosmo_spec.compiler.name + cosmo_spec.format('{^mpi.variants}')
+    dycore_spec += ' ^' + cosmo_spec.format('{^mpi.name}') + cosmo_spec.format('{^mpi.@version}') + cosmo_spec.format('{^mpi.%compiler}') + cosmo_spec.format('{^mpi.variants}')
 
     # remove the slurm_args variant causing troubles to the concretizer
     dycore_spec = dycore_spec.replace(cosmo_spec.format('{^cosmo-dycore.variants.slurm_args}'), ' ')
