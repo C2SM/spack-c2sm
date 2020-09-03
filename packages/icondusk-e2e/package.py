820673943d7e9d92f2e168c05ab61aec99c5c262
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-09-03 17:05:26 +0200

    add cmake

--- packages/icondusk-e2e/package.py
+++ packages/icondusk-e2e/package.py
@@ -9,32 +9,37 @@ import sys
 import subprocess
 import os
 
+
 class IconduskE2e(CMakePackage):
     """A library for numerical weather prediction and climate modelling"""
 
     homepage = 'https://github.com/dawn-ico/icondusk-e2e'
-    git      = 'git@github.com:dawn-ico/icondusk-e2e.git'
+    git = 'git@github.com:dawn-ico/icondusk-e2e.git'
     maintainers = ['cosunae']
 
     version('master', branch='master')
 
-    depends_on('atlas_utilities', type=('build','run'))
-    depends_on('dawn4py',  type=('build','run'))
+    depends_on('atlas_utilities', type=('build', 'run'))
+    depends_on('dawn4py',  type=('build', 'run'))
     depends_on('python@3.8.0')
     depends_on('atlas')
+    depends_on('cmake@3.17.0')
     depends_on('cuda', type=('build', 'run'))
 
-    variant('build_type', default='Release', description='Build type', values=('Debug', 'Release', 'DebugRelease'))
-    variant('precision', default='double', values=('double','float'))
+    variant('build_type', default='Release', description='Build type',
+            values=('Debug', 'Release', 'DebugRelease'))
+    variant('precision', default='double', values=('double', 'float'))
 
     def cmake_args(self):
         args = []
         spec = self.spec
 
-
-        gcclibdir = os.path.dirname(subprocess.run([self.compiler.cxx,"-print-file-name=libstdc++.so"], capture_output=True).stdout.decode("utf-8"))
-        args.append('-DCMAKE_BUILD_TYPE={0}'.format(self.spec.variants['build_type'].value))
-        args.append('-DPython3_EXECUTABLE=' + spec['python'].prefix +'/bin/python3.8')
+        gcclibdir = os.path.dirname(subprocess.run(
+            [self.compiler.cxx, "-print-file-name=libstdc++.so"], capture_output=True).stdout.decode("utf-8"))
+        args.append(
+            '-DCMAKE_BUILD_TYPE={0}'.format(self.spec.variants['build_type'].value))
+        args.append('-DPython3_EXECUTABLE=' +
+                    spec['python'].prefix + '/bin/python3.8')
         args.append('-Datlas_utils_ROOT='+spec['atlas_utilities'].prefix)
         args.append('-Ddawn4py_DIR='+spec['dawn4py'].prefix)
         args.append('-Datlas_DIR='+spec['atlas'].prefix)
@@ -44,4 +49,3 @@ class IconduskE2e(CMakePackage):
         args.append('-DCMAKE_INSTALL_RPATH='+gcclibdir)
 
         return args
-
