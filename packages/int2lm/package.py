e4b595091a8b58b438fac1f2d3a6b9a5df2bfcbb
elsagermann <57254519+elsagermann@users.noreply.github.com>
2020-09-03 15:51:48 +0200

    FIX: +pollen true per default

--- packages/int2lm/package.py
+++ packages/int2lm/package.py
@@ -53,7 +53,7 @@ class Int2lm(MakefilePackage):
     variant('debug', default=False, description='Build debug INT2LM')
     variant('eccodes', default=True, description='Build with eccodes instead of grib-api')
     variant('parallel', default=True, description='Build parallel INT2LM')
-    variant('pollen', default=False, description='Build with pollen enabled')
+    variant('pollen', default=True, description='Build with pollen enabled')
     variant('slave', default='tsa', description='Build on slave tsa, daint or kesch', multi=False)
     variant('verbose', default=False, description='Build with verbose enabled')
 
