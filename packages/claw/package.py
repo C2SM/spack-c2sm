4a67955a4284fcee437fea56614ef5f19e08f65c
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-09-03 16:40:26 +0200

    Added support for CLAW v2.1.RC

--- packages/claw/package.py
+++ packages/claw/package.py
@@ -13,9 +13,10 @@ class Claw(CMakePackage):
 
     homepage = 'https://claw-project.github.io/'
     git      = 'https://github.com/claw-project/claw-compiler.git'
-    maintainers = ['clementval']
+    maintainers = ['FrostyMike']
 
     version('master', branch='master', submodules=True)
+    version('2.1-rc', branch='v2.1-rc')
     version('2.0.1', commit='f5acc929df74ce66a328aa4eda9cc9664f699b91', submodules=True)
     version('2.0',   commit='53e705b8bfce40a5c5636e8194a7622e337cf4f5', submodules=True)
     version('1.2.3', commit='eaf5e5fb39150090e51bec1763170ce5c5355198', submodules=True)
@@ -23,30 +24,38 @@ class Claw(CMakePackage):
     version('1.2.1', commit='939989ab52edb5c292476e729608725654d0a59a', submodules=True)
     version('1.2.0', commit='fc9c50fe02be97b910ff9c7015064f89be88a3a2', submodules=True)
     version('1.1.0', commit='16b165a443b11b025a77cad830b1280b8c9bcf01', submodules=True)
-    
-    variant('omni-master', default=False, description='Build with the master version of the omni-compiler')
 
-    depends_on('cmake@3.0:%gcc', type='build')
-    depends_on('java@8:', when="@2.0:")
-    depends_on('java@7:', when="@1.1.0:1.2.3")
+    variant('omni-master', default=False, description='Build with the latest claw-project/xcodeml-tools')
+
+    depends_on('xcodeml-tools@latest', when='@2.1:+omni-master')
+
+    depends_on('xcodeml-tools@92a35f9', when='@2.1~omni-master')
+
+    depends_on('cmake@3.16.3:', type='build')
+    depends_on('java@8:', when='@2.0:')
+    depends_on('java@7:', when='@1.1.0:1.2.3')
     depends_on('ant@1.9:%gcc')
-    depends_on('libxml2%gcc')
-    depends_on('bison%gcc')
-    depends_on('flex%gcc')
-    
+    depends_on('libxml2%gcc', when='@1.1.0:2.0.1')
+    depends_on('bison%gcc', when='@1.1.0:2.0.1')
+    depends_on('flex%gcc', when='@1.1.0:2.0.1')
+
     def setup_environment(self, spack_env, run_env):
-        spack_env.set('YACC', 'bison -y')
+        if self.version < Version('2.1'):
+            spack_env.set('YACC', 'bison -y')
+
     def cmake_args(self):
         args = []
         spec = self.spec
-        
-        if spec.variants['omni-master'].value:
-            args.append('-DOMNI_GIT_HASH=master')
-
-        args.append('-DOMNI_CONF_OPTION=--with-libxml2={0}'.
-                    format(spec['libxml2'].prefix))
-
+        if self.version >= Version('2.1'):
+            args += ['-DJAVA_HOME=' + self.spec['java'].prefix,
+                     '-DADD_OMNI_XCODEML_TOOLS_TO_INSTALL=OFF',
+                     '-DBUILD_OMNI_XCODEML_TOOLS=OFF',
+                     '-DOMNI_HOME=' + self.spec['xcodeml-tools'].prefix]
+        else:
+            if spec.variants['omni-master'].value:
+                args.append('-DOMNI_GIT_HASH=master')
+            args.append('-DOMNI_CONF_OPTION=--with-libxml2={0}'.
+                        format(spec['libxml2'].prefix))
         args.append('-DCMAKE_Fortran_COMPILER={0}'.
                     format(self.compiler.fc))
-
         return args
