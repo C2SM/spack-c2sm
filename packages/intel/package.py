b9219dabe4ea5238223410e4d8ea8bf693f2fae1
Mikhail Zhigun <mzhigun@daint102.login.cscs.ch>
2020-09-17 10:48:23 +0200

    Added gcc package on daint

--- packages/intel/package.py
+++ packages/intel/package.py
@@ -8,8 +8,6 @@ class Intel(IntelPackage):
 
     homepage = "https://software.intel.com/en-us/intel-parallel-studio-xe"
 
-    # Same as in ../intel-parallel-studio/package.py, Composer Edition,
-    # but the version numbering in Spack differs.
     version('19.0.1.144', sha256='none', url='http://fake-link.tgz')
     version('19.1.1.217', sha256='none', url='http://fake-link.tgz')
 
