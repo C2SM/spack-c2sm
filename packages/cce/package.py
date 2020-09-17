aac17136e954e7ff1cc01fc42919685c7adb8196
Mikhail Zhigun <mzhigun@tsa-ln002.cm.cluster>
2020-09-17 12:12:14 +0200

    Fixed compiler packages on tsa

--- packages/cce/package.py
+++ packages/cce/package.py
@@ -7,6 +7,7 @@ class Cce(Package):
     Note: This package cannot be installed, it wraps existing installation, specified 
     in packages.yaml.
     """
-
+    
+    homepage = "https://user.cscs.ch/computing/compilation/cray/"
     version('10.0.2', sha256='none', url='http://fake-link.tgz')
 
