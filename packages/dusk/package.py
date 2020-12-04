bb1d2013a2a2ad0917dd804008b9d724fb391619
Stagno <giacomo.serafini@mail.polimi.it>
2020-12-04 09:55:15 +0100

    Add dusk package (#143)
    As a PythonPackage. Depends on dawn4py and python@3.8.X.

---
+++ packages/dusk/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+#
+from spack import *
+import sys
+
+class Dusk(PythonPackage):
+    """A minimal and lightweight front-end for dawn."""
+
+    homepage = 'https://github.com/dawn-ico/dusk'
+    git      = 'https://github.com/dawn-ico/dusk'
+    maintainers = ['BenWeber42']
+
+    version('master', branch='master')
+    
+    extends('python@3.8.0:3.8.999')
+
+    depends_on('dawn4py', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+
+    # will test that dusk is importable after the install
+    import_modules = ['dusk']
