ebe42fd58239803517f4c8ec2630d09746a21b04
Guy de Morsier <guy.demorsier@meteoswiss.ch>
2021-02-02 07:56:39 +0100

    Use lib_dir so it can be also used for GRIBAPI

--- packages/int2lm/package.py
+++ packages/int2lm/package.py
@@ -64,15 +64,16 @@ class Int2lm(MakefilePackage):
         if '~eccodes' in self.spec:
             grib_prefix = self.spec['cosmo-grib-api'].prefix
             grib_lib_names = '-lgrib_api_f90 -lgrib_api'
+            lib_dir='/lib'
         else:
             grib_prefix = self.spec['eccodes'].prefix
             grib_lib_names = '-leccodes_f90 -leccodes'
             # Default installation lib path changed to from lib to lib64 after 2.19.0
             if self.spec['eccodes'].version >= Version('2.19.0'):
-                eccodes_lib_dir='/lib64'
+                lib_dir='/lib64'
             else:
-                eccodes_lib_dir='/lib'
-        env.set('GRIBAPIL', '-L' + grib_prefix + eccodes_lib_dir + grib_lib_names + ' -L' + self.spec['jasper'].prefix + '/lib64 -ljasper')
+                lib_dir='/lib'
+        env.set('GRIBAPIL', '-L' + grib_prefix + lib_dir + grib_lib_names + ' -L' + self.spec['jasper'].prefix + '/lib64 -ljasper')
         env.set('GRIBAPII', '-I' + grib_prefix + '/include')
 
         # Netcdf library
