687ab1520a1652876caa1ba5b0adda294ceb65b4
Jonas Jucker <joni.jucker@yahoo.de>
2021-03-01 09:10:02 +0100

    CSCS-support suggestions

--- packages/icontools/package.py
+++ packages/icontools/package.py
@@ -21,7 +21,6 @@
 # ----------------------------------------------------------------------------
 
 from spack import *
-import os
 
 
 class Icontools(AutotoolsPackage):
@@ -44,12 +43,13 @@ class Icontools(AutotoolsPackage):
     depends_on('libtool',  type='build')
     depends_on('m4', type='build')
 
-    depends_on('netcdf-fortran', type=('build', 'link'))
-    depends_on('netcdf-c', type=('build', 'link'))
+    depends_on('cray-libsci %cce', type=('build', 'link'))
+    depends_on('netcdf-fortran %cce', type=('build', 'link'))
+    depends_on('netcdf-c %cce ~mpi', type=('build', 'link'))
     depends_on('mpi', type=('build', 'link', 'run'),)
     depends_on('eccodes ~aec', type=('build', 'link', 'run'))
     depends_on('cosmo-grib-api', type=('build','link','run'), when='~eccodes')
-    depends_on('hdf5', type=('build','link'))
+    depends_on('hdf5 ~mpi +hl', type=('build','link'))
     depends_on('jasper@1.900.1%gcc ~shared', type=('build','link'))
 
     variant('eccodes', default=True, description='Build with eccodes instead of grib-api')
@@ -57,7 +57,7 @@ class Icontools(AutotoolsPackage):
     def configure_args(self):
         args =['--disable-silent-rules',
                '--disable-shared',
-               '--with-netcdf={0}/CRAYCLANG/9.0'.format(self.spec['netcdf-fortran'].prefix),
+               '--with-netcdf={0}'.format(self.spec['netcdf-fortran'].prefix),
                '--enable-iso-c-interface',
                 ]
 
@@ -66,48 +66,47 @@ class Icontools(AutotoolsPackage):
         else:
             args.append('--enable-grib2')
             args.append('--with-eccodes={0}'.format(self.spec['eccodes'].prefix))
-        
+
         return args
 
     def setup_build_environment(self, env):
         self.setup_run_environment(env)
+        #env.set('NETCDF_DIR','{}'.format(self.spec['netcdf-c'].prefix))
+        #env.set('HDF5_ROOT','{}'.format(self.spec['hdf5'].prefix))
 
         # construct all includes/libraries for **FLAGS
         include = ''
         libs = ''
 
         # NetCDF
-        #netcdf_root = os.getenv('NETCDF_DIR')
-        #include +=' -I{}/include'.format(netcdf_root)
-        #libs +=' -L{}/lib -lnetcdff'.format(netcdf_root)
-        include += ' -L{}/CRAYCLANG/9.0/include'.format(self.spec['netcdf-fortran'].prefix)
-        #include +=' -I{}/include'.format(self.spec['netcdf-fortran'].prefix)
+        ###MKr include +=' -I{}/include'.format(self.spec['netcdf-fortran'].prefix)
+        ###MKr libs =' -L{}/lib -lnetcdff'.format(self.spec['netcdf-fortran'].prefix)
 
-        #libs =' -L{}/lib -lnetcdff'.format(self.spec['netcdf-fortran'].prefix)
-        libs += ' -L{}/CRAYCLANG/9.0/lib -lnetcdff'.format(self.spec['netcdf-fortran'].prefix)
+        ###MKr include +=' -I{}/include'.format(self.spec['netcdf-c'].prefix)
+        ###MKr libs +=' -L{}/lib -lnetcdf '.format(self.spec['netcdf-c'].prefix)
+        include +=' -I{}/include'.format(self.spec['netcdf-c'].prefix)
+        libs +=' -L{}/lib '.format(self.spec['netcdf-c'].prefix)
+        env.set('NETCDF_DIR','{}'.format(self.spec['netcdf-c'].prefix))
 
-
-        include += ' -L{}/CRAYCLANG/9.0/include'.format(self.spec['netcdf-c'].prefix)
-        #include +=' -I{}/include'.format(self.spec['netcdf-c'].prefix)
-
-        #libs +=' -L{}/lib -lnetcdf '.format(self.spec['netcdf-c'].prefix)
-        libs += ' -L{}/CRAYCLANG/9.0/lib -lnetcdf '.format(self.spec['netcdf-c'].prefix)
-        
         # MPI
         include +=' -I{}/include'.format(self.spec['mpi'].prefix)
         libs +=' -L{}/lib '.format(self.spec['mpi'].prefix)
 
         # Jasper
+        ###MKr include +=' -I{}/include'.format(self.spec['jasper'].prefix)
+        ###MKr libs +=' -L{}/lib -ljasper'.format(self.spec['jasper'].prefix)
         include +=' -I{}/include'.format(self.spec['jasper'].prefix)
-        libs +=' -L{}/lib -ljasper'.format(self.spec['jasper'].prefix)
+        libs +=' -L{}/lib '.format(self.spec['jasper'].prefix)
 
         #HDF5
         libs +=' -L{}/lib '.format(self.spec['hdf5'].prefix)
 
         # Grib-Api/Eccodes
         if '~eccodes' in self.spec:
+            ###MKr include +=' -I{}/include'.format(self.spec['cosmo-grib-api'].prefix)
+            ###MKr libs +=' -L{}/lib -lgrib_api_f90 -lgrib_api'.format(self.spec['cosmo-grib-api'].prefix)
             include +=' -I{}/include'.format(self.spec['cosmo-grib-api'].prefix)
-            libs +=' -L{}/lib  -lgrib_api_f90 -lgrib_api'.format(self.spec['cosmo-grib-api'].prefix)
+            libs +=' -L{}/lib '.format(self.spec['cosmo-grib-api'].prefix)
         else:
             include +=' -I{}/include'.format(self.spec['eccodes'].prefix)
 
@@ -116,19 +115,24 @@ class Icontools(AutotoolsPackage):
             else:
                 eccodes_lib_dir='lib'
 
-            libs +=' -L{}/{} -leccodes -leccodes_f90'.format(self.spec['eccodes'].prefix, eccodes_lib_dir)
+            ###MKr libs +=' -L{}/{} -leccodes -leccodes_f90'.format(self.spec['eccodes'].prefix, eccodes_lib_dir)
+            libs +=' -L{}/{} '.format(self.spec['eccodes'].prefix, eccodes_lib_dir)
 
+        # Cray Libsci
+        include += ' -I{}/include'.format(self.spec['cray-libsci'].prefix)
+        libs += ' -L{}/lib'.format(self.spec['cray-libsci'].prefix)
 
         flags = ' -O2 -g -Wunused  -DHAVE_LIBNETCDF -DHAVE_NETCDF4 -DHAVE_CF_INTERFACE -DHAVE_LIBGRIB -DHAVE_LIBGRIB_API -D__ICON__ -DNOMPI'
 
-        cflags = include + ' ' + libs + ' ' + flags
+        cflags = include + flags
         env.set('CFLAGS', cflags)
 
-        cxxflags = include 
+        cxxflags = '-fopenmp '
+        cxxflags += include
         cxxflags += '-Wunused -DNOMPI'
         env.set('CXXLAGS', cxxflags)
 
-        fcflags = include 
+        fcflags = include
         fcflags += ' -O2 -g -cpp -Wunused -DNOMPI'
         env.set('FCFLAGS', fcflags)
 
@@ -137,19 +141,18 @@ class Icontools(AutotoolsPackage):
 
         libs_env = ''
 
-        #libs_env += ' -L{}/lib -lnetcdf '.format(self.spec['netcdf-c'].prefix)
-        #libs_env += ' -L{}/lib -lnetcdff'.format(self.spec['netcdf-fortran'].prefix)
-
-        libs_env += ' -L{}/CRAYCLANG/9.0/lib -lnetcdf '.format(self.spec['netcdf-c'].prefix)
-        libs_env += ' -L{}/CRAYCLANG/9.0/lib -lnetcdff'.format(self.spec['netcdf-fortran'].prefix)
+        libs_env += ' -lhdf5 -lnetcdf -lnetcdff'
 
         if '~eccodes' in self.spec:
             libs_env += ' -lgrib_api  -lgrib_api_f90'
         else:
             libs_env += ' -leccodes -leccodes_f90'
-        
+
         # needs to be after griblibs, otherwise linking error during configure
         libs_env += ' -ljasper'
+
+        # Cray Libsci
+        libs_env += ' -lsci_cray'
 
         env.set('LIBS', libs_env)
 
