d59c939b4746183969d4b97f1458f7fb670656ec
Jonas Jucker <joni.jucker@yahoo.de>
2021-03-03 11:01:00 +0100

    implement another suggestion from M.Kraushaar

--- packages/icontools/package.py
+++ packages/icontools/package.py
@@ -47,12 +47,17 @@ class Icontools(AutotoolsPackage):
     variant('eccodes', default=True, description='Build with eccodes instead of grib-api')
 
     def configure_args(self):
-        args =['--disable-silent-rules',
-               '--disable-shared',
-               '--with-netcdf={0}'.format(self.spec['netcdf-fortran'].prefix),
-               '--enable-iso-c-interface',
-                ]
-
+        args =[]
+        args.append('CC=cc')
+        args.append('CXX=CC')
+        args.append('FC=ftn')
+        args.append('F77=ftn')
+        args.append('acx_cv_fc_ftn_include_flag="-I"')
+        args.append('acx_cv_fc_pp_include_flag="-I"')
+        args.append('--disable-silent-rules')
+        args.append('--disable-shared')
+        args.append('--with-netcdf={0}'.format(self.spec['netcdf-fortran'].prefix))
+        args.append('--enable-iso-c-interface')
         if '~eccodes' in self.spec:
             args.append('--with-grib_api={0}'.format(self.spec['cosmo-grib-api'].prefix))
         else:
@@ -62,85 +67,43 @@ class Icontools(AutotoolsPackage):
         return args
 
     def setup_build_environment(self, env):
-        self.setup_run_environment(env)
+        # Daint specific flags since cray-modules setting not recognized
+        env.set('NETCDF_DIR', '{}'.format(self.spec['netcdf-c'].prefix))
 
-        # construct all includes/libraries for **FLAGS
-        include = ''
-        libs = ''
-
-        include +=' -I{}/include'.format(self.spec['netcdf-c'].prefix)
-        libs +=' -L{}/lib '.format(self.spec['netcdf-c'].prefix)
-        env.set('NETCDF_DIR','{}'.format(self.spec['netcdf-c'].prefix))
-
-        # MPI
-        include +=' -I{}/include'.format(self.spec['mpi'].prefix)
-        libs +=' -L{}/lib '.format(self.spec['mpi'].prefix)
-
-        # Jasper
-        include +=' -I{}/include'.format(self.spec['jasper'].prefix)
-        libs +=' -L{}/lib '.format(self.spec['jasper'].prefix)
-
-        #HDF5
-        libs +=' -L{}/lib '.format(self.spec['hdf5'].prefix)
-
-        # Grib-Api/Eccodes
+        #Setting CFLAGS
+        env.append_flags('CFLAGS', '-O2')
+        env.append_flags('CFLAGS', '-g')
+        env.append_flags('CFLAGS', '-Wunused')
+        env.append_flags('CFLAGS', '-DHAVE_LIBNETCDF')
+        env.append_flags('CFLAGS', '-DHAVE_NETCDF4')
+        env.append_flags('CFLAGS', '-DHAVE_CF_INTERFACE')
+        env.append_flags('CFLAGS', '-DHAVE_LIBGRIB_API')
+        env.append_flags('CFLAGS', '-D__ICON__')
+        env.append_flags('CFLAGS', '-DNOMPI')
+        #Setting CXXFLAGS
+        env.append_flags('CXXFLAGS', '-O2')
+        env.append_flags('CXXFLAGS', '-g')
+        env.append_flags('CXXFLAGS', '-fopenmp')
+        env.append_flags('CXXFLAGS', '-Wunused')
+        env.append_flags('CXXFLAGS', '-DNOMPI')
+        #Setting FCFLAGS
+        env.append_flags('FCFLAGS', '-O2')
+        env.append_flags('FCFLAGS', '-g')
+        env.append_flags('FCFLAGS', '-cpp')
+        env.append_flags('FCFLAGS', '-Wunused')
+        env.append_flags('FCFLAGS', '-DNOMPI')
+        #Setting LIBS
+        env.append_flags('LIBS', '-lhdf5')
+        env.append_flags('LIBS', '-ljasper')
+        env.append_flags('LIBS', '-lsci_cray')
         if '~eccodes' in self.spec:
-            include +=' -I{}/include'.format(self.spec['cosmo-grib-api'].prefix)
-            libs +=' -L{}/lib '.format(self.spec['cosmo-grib-api'].prefix)
+            env.append_flags('LIBS', '-lgrib_api')
+            env.append_flags('LIBS', '-lgrib_api_f90')
         else:
-            include +=' -I{}/include'.format(self.spec['eccodes'].prefix)
-
-            if self.spec['eccodes'].version >= Version('2.19.0'):
-                eccodes_lib_dir='lib64'
-            else:
-                eccodes_lib_dir='lib'
-
-            libs +=' -L{}/{} '.format(self.spec['eccodes'].prefix, eccodes_lib_dir)
-
-        # Cray Libsci
-        include += ' -I{}/include'.format(self.spec['cray-libsci'].prefix)
-        libs += ' -L{}/lib'.format(self.spec['cray-libsci'].prefix)
-
-        flags = ' -O2 -g -Wunused  -DHAVE_LIBNETCDF -DHAVE_NETCDF4 -DHAVE_CF_INTERFACE -DHAVE_LIBGRIB -DHAVE_LIBGRIB_API -D__ICON__ -DNOMPI'
-
-        cflags = include + flags
-        env.set('CFLAGS', cflags)
-
-        cxxflags = '-fopenmp '
-        cxxflags += include
-        cxxflags += '-Wunused -DNOMPI'
-        env.set('CXXLAGS', cxxflags)
-
-        fcflags = include
-        fcflags += ' -O2 -g -cpp -Wunused -DNOMPI'
-        env.set('FCFLAGS', fcflags)
-
-        ldflags = libs
-        env.set('LDFLAGS', ldflags)
-
-        libs_env = ''
-
-        libs_env += ' -lhdf5 -lnetcdf -lnetcdff'
-
-        if '~eccodes' in self.spec:
-            libs_env += ' -lgrib_api  -lgrib_api_f90'
-        else:
-            libs_env += ' -leccodes -leccodes_f90'
-
-        # needs to be after griblibs, otherwise linking error during configure
-        libs_env += ' -ljasper'
-
-        # Cray Libsci
-        libs_env += ' -lsci_cray'
-
-        env.set('LIBS', libs_env)
-
-        # Daint specific flags to cache
-        env.set('acx_cv_fc_ftn_include_flag', '-I')
-        env.set('acx_cv_fc_pp_include_flag', '-I')
+            env.append_flags('LIBS', '-leccodes')
+            env.append_flags('LIBS', '-leccodes_f90')
 
     @run_after('build')
-    @on_package_attributes(run_tests=True)
     def test(self):
             try:
                 subprocess.run(['/bin/bash', 'C2SM-scripts/test/jenkins/test.sh'], stderr=subprocess.STDOUT, check=True)
