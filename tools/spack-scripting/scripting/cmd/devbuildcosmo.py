da781a5100408f2f57051f7fddbce8ac55eeee34
cosunae <carlos.osuna@env.ethz.ch>
2021-03-13 23:05:10 +0100

    get dependencies from yaml

--- tools/spack-scripting/scripting/cmd/devbuildcosmo.py
+++ tools/spack-scripting/scripting/cmd/devbuildcosmo.py
@@ -9,6 +9,7 @@ import subprocess
 import shutil
 
 import llnl.util.tty as tty
+import re
 
 import spack.config
 import spack.cmd
@@ -18,6 +19,8 @@ from spack.stage import DIYStage
 from spack.spec import Spec
 from spack.cmd.dev_build import dev_build
 from spack.main import SpackCommand
+
+from buildconf import BuildConf
 
 description = "Dev-build cosmo and dycore with or without testing."
 section = "scripting"
@@ -46,17 +49,35 @@ def devbuildcosmo(self, args):
     if not args.spec:
         tty.die("spack dev-build requires a package spec argument.")
 
+    buildcfg = BuildConf()
+    spec_str = ''.join(args.spec)
+    for dep in buildcfg.depsconf_.parameters_.keys():
+        print(dep, args.spec)
+        if re.search('\^'+dep, spec_str):
+            print("Error: found in spec a dependency key:", dep, " that is already specified in dependencies.yaml " )
+            sys.exit(1)
+
+    dep_spec = buildcfg.dependency_spec
+    # Remove the reference the dycore in the spec, since it will be devbuild
+    dep_spec = re.sub(r'\^cosmo-dycore@[^ ]*','',dep_spec)
+    for aspec in dep_spec.split():
+        args.spec.append(aspec)
+    args.spec.append('^cosmo-dycore@dev-build')
+
     specs = spack.cmd.parse_specs(args.spec)
+ 
     if len(specs) > 1:
         tty.die("spack dev-build only takes one spec.")
 
     cosmo_spec = specs[0]
+
     cosmo_spec.concretize()
 
     # Setting source_path to current working directory
     source_path = os.getcwd()
     source_path = os.path.abspath(source_path)
 
+   
     # Clean if needed
     if args.clean_build:
         print('\033[92m' + '==> ' + '\033[0m' + 'cosmo: Cleaning build directory')
@@ -67,9 +88,6 @@ def devbuildcosmo(self, args):
             shutil.rmtree(source_path + '/spack-build')
 
     if cosmo_spec.satisfies('+cppdycore'):
-        # Set dycore_spec
-        dycore_spec = 'cosmo-dycore@dev-build'
-
         # Extracting dycore variants
         dycore_spec = cosmo_spec.format('{^cosmo-dycore.name}') + cosmo_spec.format('{^cosmo-dycore.@version}') + cosmo_spec.format('{^cosmo-dycore.%compiler}') + cosmo_spec.format('{^cosmo-dycore.variants}')
 
@@ -81,12 +99,16 @@ def devbuildcosmo(self, args):
 
         dycore_spec = Spec(dycore_spec).concretized()
 
+        print("Full spec", dycore_spec)
         custom_devbuild(source_path, dycore_spec, args.jobs)
         
         # Launch dycore tests
         if args.test:
             print('\033[92m' + '==> ' + '\033[0m' + 'cosmo-dycore: Launching dycore tests')
             subprocess.run(['./dycore/test/tools/test_dycore.py', str(dycore_spec), source_path + '/spack-build'])
+
+   
+
 
     # Dev-build cosmo
     custom_devbuild(source_path, cosmo_spec, args.jobs)
