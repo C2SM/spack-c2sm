89518f065f1f359a29b954227b41a0582466a5b7
Guy de Morsier <guy.demorsier@meteoswiss.ch>
2021-04-14 15:48:25 +0200

    unify all int2lm github repos (#197)
    This PR brings a single spack int2lm package for COSMO-ORG/int2lm, C2SM-RCM/int2lm and MeteoSwiss-APN/int2lm.
    Use now int2lm@org_master, apn_master and int2lm@c2sm_master to get the different repos. and use int2lm@org_2.08, int2lm@apn_v2.8.3 and int2lm@c2sm_v2.8.3 for the latest tags.

--- packages/int2lm/package.py
+++ packages/int2lm/package.py
@@ -4,22 +4,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 # ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install int2lm
-#
-# You can edit this file again by typing:
-#
-#     spack edit int2lm
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 import os
@@ -30,20 +14,38 @@ class Int2lm(MakefilePackage):
     and/or boundary data for the COSMO-Model."""
 
     homepage = "http://www.cosmo-model.org/content/model/"
-    url      = "https://github.com/MeteoSwiss-APN/int2lm/archive/v2.7.2.tar.gz"
+    url      = "https://github.com/MeteoSwiss-APN/int2lm/archive/v2.8.3.tar.gz"
     git      = 'git@github.com:MeteoSwiss-APN/int2lm.git'
+    c2smgit  = 'git@github.com:C2SM-RCM/int2lm.git'
+    orggit  = 'git@github.com:COSMO-ORG/int2lm.git'
+
 
     maintainers = ['morsier']
 
-    version('master', branch='master')
     version('dev-build', branch='master')
-    version('v2.8.3', commit='43796aa0a2c56071efc3277397abbbf78dab1247')
-    version('v2.8.2', commit='7f8bf2e3f5e77489cfdb4443578a43431408e2bd')
-    version('v2.8.1', commit='844d239cfa83bc9980696cae56f47da3d08ce4ec')
-    version('v2.7.2', commit='7a460906e826142be1fb9338d2210ccf7566d5a2')
-    version('v2.7.1_p2', commit='05e2a405a66f62706df17f01bbc463d0c365e168')
-    version('v2.7.1', commit='ee0780f86ecc676a9650170f361b92ff93379071')
-    version('v2.6.2', commit='07690dab05c931ba02c947ec32c988eea65898f8')
+
+    # APN tags
+    version('apn_master', git=git, branch='master')
+    version('apn_v2.8.3', git=git, commit='43796aa0a2c56071efc3277397abbbf78dab1247')
+    version('apn_v2.8.2', git=git, commit='7f8bf2e3f5e77489cfdb4443578a43431408e2bd')
+    version('apn_v2.8.1', git=git, commit='844d239cfa83bc9980696cae56f47da3d08ce4ec')
+    version('apn_v2.7.2', git=git, commit='7a460906e826142be1fb9338d2210ccf7566d5a2')
+    version('apn_v2.7.1_p2', git=git, commit='05e2a405a66f62706df17f01bbc463d0c365e168')
+    version('apn_v2.7.1', git=git, commit='ee0780f86ecc676a9650170f361b92ff93379071')
+    version('apn_v2.6.2', git=git, commit='07690dab05c931ba02c947ec32c988eea65898f8')
+
+    # C2SM tags
+    version('c2sm_master', git=c2smgit, branch='master')
+    version('c2sm_v2.8.3', git=c2smgit, commit='da56842f2222d241ecc129f95ef097a6773dfe90')
+    version('c2sm_v2.8.2', git=c2smgit, commit='7f8bf2e3f5e77489cfdb4443578a43431408e2bd')
+
+    # ORG tags
+    version('org_master', git=orggit, branch='master')
+    version('org_2.08', git=orggit, commit='9e0d0bfe50f8e29676c7d1f0f4205597be8e86e1')
+    version('org_2.07', git=orggit, commit='65ddb3af9b7d63fa2019d8bcee41e8d4a99baedd')
+    version('org_2.06a', git=orggit, commit='eb067a01446f55e1b55f6341681e97a95f856865')
+    version('org_2.06', git=orggit, commit='11065ff1b304129ae19e774ebde02dcd743d2005')
+    version('org_2.05', git=orggit, commit='ef16f54f53401e99aef083c447b4909b8230a4a0')
 
     depends_on('cosmo-grib-api-definitions', type=('build','run'), when='~eccodes')
     depends_on('cosmo-eccodes-definitions ~aec', type=('build','run'), when='+eccodes')
@@ -56,8 +58,13 @@ class Int2lm(MakefilePackage):
     variant('eccodes', default=True, description='Build with eccodes instead of grib-api')
     variant('parallel', default=True, description='Build parallel INT2LM')
     variant('pollen', default=True, description='Build with pollen enabled')
-    variant('slave', default='tsa', description='Build on slave tsa, daint or kesch', multi=False)
+    variant('slave', default='tsa', description='Build on slave tsa or daint', multi=False)
     variant('verbose', default=False, description='Build with verbose enabled')
+
+    conflicts('pollen=True' , when='@org_master,org_2.05:org_2.08', 
+              msg='int2lm-org is currently broken with pollen, set variant pollen=False')
+
+    build_directory='TESTSUITE'
 
     def setup_build_environment(self, env):
         self.setup_run_environment(env)
@@ -135,21 +142,35 @@ class Int2lm(MakefilePackage):
         return build
 
     def edit(self, spec, prefix):
-        makefile = FileFilter('Makefile')
-        OptionsFileName= 'Options'
-        if self.compiler.name == 'gcc':
-            OptionsFileName += '.gnu'
-        elif self.compiler.name == 'pgi':
-            OptionsFileName += '.pgi'
-        elif self.compiler.name == 'cce':
-            OptionsFileName += '.cray'
-        makefile.filter('/Options.*', '/' + OptionsFileName)
+        print(os.getcwd())
+        with working_dir(self.build_directory):
+            makefile = FileFilter('Makefile')
+            OptionsFileName= 'Options'
+            if self.compiler.name == 'gcc':
+                OptionsFileName += '.gnu'
+            elif self.compiler.name == 'pgi':
+                OptionsFileName += '.pgi'
+            elif self.compiler.name == 'cce':
+                OptionsFileName += '.cray'
+            makefile.filter('/Options.*', '/' + OptionsFileName)
 
     def install(self, spec, prefix):
-        mkdir(prefix.bin)
-        mkdir(prefix.test)
-        install('int2lm', prefix.bin)
-        install('int2lm', 'test/testsuite')
+            with working_dir(self.build_directory):
+                install('int2lm', prefix.bin)
+                install('int2lm', '../test/testsuite')
+
+    @run_before('edit')
+    def create_build_directory(self):
+        os.makedirs(self.build_directory,exist_ok=True)
+
+        # link all files for APN and C2SM version of int2lm
+        if not os.path.isfile(self.build_directory + '/Options.tsa.pgi'):
+            os.chdir(self.build_directory)
+
+            for item in os.listdir('../'):
+                os.symlink('../' + item, item)
+
+            os.chdir('..')
 
     @run_after('install')
     @on_package_attributes(run_tests=True)
