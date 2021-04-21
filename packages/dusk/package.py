72557ff1cf9e29645422bfde67c2344ec32dceec
cosunae <carlos.osuna@meteoswiss.ch>
2021-04-21 21:54:34 +0200

    fix typo

--- packages/dusk/package.py
+++ packages/dusk/package.py
@@ -15,7 +15,7 @@ class Dusk(PythonPackage):
     maintainers = ['BenWeber42']
 
     version('master', branch='master')
-    version('horizon', branch='horizon', preferred=True))
+    version('horizon', branch='horizon', preferred=True)
 
     extends('python@3.8.0:3.8.999')
 
