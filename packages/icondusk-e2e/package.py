4cc51ee6a682e74c310c1410e989da958ba153cc
cosunae <carlos.osuna@meteoswiss.ch>
2021-04-28 14:23:11 +0200

    dusk & iconbdusk-e2e: add horizon and icondusk-e2e versions

--- packages/icondusk-e2e/package.py
+++ packages/icondusk-e2e/package.py
@@ -18,7 +18,6 @@ class IconduskE2e(CMakePackage):
     maintainers = ['cosunae']
 
     version('master', branch='master')
-    version('0.1-dev', tag='0.1-dev')
 
     depends_on('cmake@3.17.0:')
     depends_on('boost@1.73.0%gcc')
@@ -26,8 +25,7 @@ class IconduskE2e(CMakePackage):
     depends_on('netcdf-c')
     depends_on('atlas_utilities', type=('build', 'run'))
     depends_on('dawn4py',  type=('build'))
-    depends_on('dusk',  type=('build'))
-    depends_on('dusk@icondusk-e2e',  type=('build'), when='@0.1-dev')
+    depends_on('dusk', type=('build'))
     depends_on('python@3.8.0:3.8.999')
     depends_on('atlas@0.22.0')
     depends_on('cuda', type=('build', 'run'))
