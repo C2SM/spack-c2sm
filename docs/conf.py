bf9157f613092fec755548e6ed961467b1cf12a0
cosunae <action@github.com>
2021-05-11 11:16:10 +0200

    fix conflicts of workflow

--- docs/conf.py
+++ docs/conf.py
@@ -58,5 +58,5 @@ html_theme_path, html_theme, needs_sphinx = set_psphinxtheme('p-green')
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
-
+master_doc = 'index'
 todo_include_todos = True
