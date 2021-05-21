2edeabd38eed681988dfb0664e7a68a6d91d64c4
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2021-05-21 13:58:21 +0200

    ADD: nwp, cscs & aes versions

--- packages/icon/package.py
+++ packages/icon/package.py
@@ -22,6 +22,9 @@ class Icon(Package):
     maintainers = ['egermann']
 
     version('master', branch='master', submodules=True)
+    version('nwp', git='git@gitlab.dkrz.de:icon/icon-nwp.git')
+    version('cscs', git='git@gitlab.dkrz.de:icon/icon-cscs.git')
+    version('aes', git='git@gitlab.dkrz.de:icon/icon-aes.git' )
     version('ham', git='git@git.iac.ethz.ch:hammoz/icon-hammoz.git', branch='hammoz/gpu/master', submodules=True)
     version('2.6.x-rc', commit='040de650', submodules=True)
     version('2.0.17', commit='39ed04ad', submodules=True)
