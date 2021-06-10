e7433c04c976f01c845b12b15dd50ae6fbbcb354
Jonas_Jucker <39263956+jonasjucker@users.noreply.github.com>
2021-06-10 17:02:41 +0200

    Bugfixes for installcosmo and devbuildcosmo (#233)
    Introducing a separate variant for all slurm arguments and options to avoid a buggy conversion to and from string.
    This solution requires changes in the cosmo repo as well, for that see:
    https://github.com/COSMO-ORG/cosmo/pull/389/files.
    
    Other changes in the PR
    Consistent use of Spack method str() to convert a String to a Spec object in cosmo and dycore package
    Refactor of -t --test option in devbuildcosmo to follow more the standard Spack way of testing
    
    * ADD: doc about  Spack commands, move parts from README.md to Sphinx-Doc
    * Update README.md
    * all Spack commands transfered to Doc from Readme
    * fix syntax errors in .rst
    * Update default.yml
    * fix links, change name of doc in conf.py
    * all doc from readme in Sphinx
    * Update README.md
    * force push in gh-action
    * doc for devbuildcosmo, installcosmo
    * Quick Guide for COSMO and int2lm
    * instructions to source preinstalled instance on Daint
    * slurm variant have "-" for unsused variants
    * multi-valued variant for slurm arguments

--- packages/cosmo-dycore/package.py
+++ packages/cosmo-dycore/package.py
@@ -58,8 +58,10 @@ class CosmoDycore(CMakePackage):
     variant('production', default=False, description='Force all variants to be the ones used in production')
     variant('cuda_arch', default='none', description='Build with cuda_arch', values=('70', '60', '37'), multi=False)
     variant('cuda', default=True, description='Build with cuda or target gpu')
-    variant('slurm_args', default='"-p debug -n {0} --gres=gpu:{0}"', description='Slurm arguments for testing')
     variant('gt1', default=False, description='Build with gridtools 1.1.3')
+
+    variant('slurm_bin', default='srun', description='Slurm binary on CSCS machines')
+    variant('slurm_args', default='None', multi=True, description='Additional arguments to submit tests')
 
     depends_on('gridtools@1.1.3 ~cuda cuda_arch=none', when='~cuda+gt1')
     depends_on('gridtools@1.1.3 +cuda', when='+cuda+gt1')
@@ -141,6 +143,6 @@ class CosmoDycore(CMakePackage):
     def test(self):
       if '+build_tests' in self.spec:
           try:
-              subprocess.run(['./test_dycore.py', '-s', str(self.spec), '-b', str(self.build_directory)], cwd = self.root_cmakelists_dir + '/test/tools', check=True, stderr=subprocess.STDOUT)
+              subprocess.run(['./test_dycore.py', '-s', self.spec.__str__(), '-b', str(self.build_directory)], cwd = self.root_cmakelists_dir + '/test/tools', check=True, stderr=subprocess.STDOUT)
           except:
               raise InstallError('Dycore tests failed')
