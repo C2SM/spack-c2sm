cc742c2edc133d46dc64bac4bc2c1eab0faaf4d2
Jonas_Jucker <39263956+jonasjucker@users.noreply.github.com>
2021-07-19 13:07:41 +0200

    introduce slurm_args for cosmo-package (#244)

--- packages/cosmo/package.py
+++ packages/cosmo/package.py
@@ -99,6 +99,19 @@ class Cosmo(MakefilePackage):
     variant('gt1', default=False, description='Build dycore with gridtools 1.1.3')
     variant('cuda_arch', default='none', description='Build with cuda_arch', values=('70', '60', '37'), multi=False)
 
+    variant('slurm_bin', default='srun', description='Slurm binary on CSCS machines')
+    variant('slurm_opt_partition', default='-p', description='Slurm option to specify partition for testing')
+    variant('slurm_partition', default='normal', description='Slurm partition for testing')
+
+    variant('slurm_opt_nodes', default='-n', description='Slurm option to specify number of nodes for testing')
+    variant('slurm_nodes', default='1', description='Pattern to specify number of nodes for testing')
+
+    variant('slurm_opt_account', default='-A', description='Slurm option to specify account for testing')
+    variant('slurm_account', default='g110', description='Slurm option to specify account for testing')
+
+    variant('slurm_opt_constraint', default='-C', description='Slurm option to specify constraints for nodes requested')
+    variant('slurm_constraint', default='gpu', description='Slurm constraints for nodes requested')
+
     conflicts('+claw', when='cosmo_target=cpu')
     conflicts('+pollen', when='@5.05:5.06,master')
     conflicts('cosmo_target=gpu', when='%gcc')
