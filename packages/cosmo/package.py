890aa6f68700248aa55e8fda6b86626b7efef8d2
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2021-08-25 16:30:11 +0200

    Replace git error message when fetching cosmo versions (#276)
    * FIX: replace git error message
    
    When fetching versions of cosmo repositories, a confusing git error
    message (like fatal ERROR) appears for repos to which the user doesn't
    have access. Replace it with a more usefull warning.
    
    * TST: test warning with dummy repo
    
    * FIX syntax error
    
    * FIX: syntax error and remove test
    
    Working fine now
    
    * FIX: use 4 spaces indent instead of 8
    
    On top of being dirty, this confuses editors, like emacs, who infer
    the python indentation from the first lines of the file.

--- packages/cosmo/package.py
+++ packages/cosmo/package.py
@@ -8,7 +8,11 @@ import subprocess, re, itertools, os
 from spack import *
 
 def get_releases(repo):
-        git_obj = subprocess.run(["git","ls-remote","--refs",repo], stdout=subprocess.PIPE)
+    git_obj = subprocess.run(["git","ls-remote","--refs",repo], capture_output=True)
+    if git_obj.returncode != 0:
+        print("\nWarning: no access to {:s} => not fetching versions\n".format(repo))
+        return []
+    else:
         git_tags = [re.match('refs/tags/(.*)', x.decode('utf-8')).group(1) for x in git_obj.stdout.split() if re.match('refs/tags/(.*)', x.decode('utf-8'))]
         return git_tags
 
@@ -17,11 +21,12 @@ def set_versions(repo, reg_filter=None):
         return re.match(reg_filter, repo_tag) != None
 
     tags = get_releases(repo)
-    if reg_filter:
-        tags = list(filter(filterfn, tags))
+    if tags:
+        if reg_filter:
+            tags = list(filter(filterfn, tags))
 
-    for tag in tags:
-        version(tag, git=repo, tag=tag, get_full_repo=True)
+        for tag in tags:
+            version(tag, git=repo, tag=tag, get_full_repo=True)
 
 
 class Cosmo(MakefilePackage):
