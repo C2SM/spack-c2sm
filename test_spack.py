ebcbe7ccceeb26fd88b0543b4263f124766c05ed
Dominic Hofer <dohofer86@gmail.com>
2021-11-15 17:51:19 +0100

    Restricts int2lm tests to tsa.

--- test_spack.py
+++ test_spack.py
@@ -176,7 +176,8 @@ class Int2lmTest(unittest.TestCase):
 
     def test_install(self):
         # So our quick start tutorial works: https://c2sm.github.io/spack-c2sm/QuickStart.html
-        run('spack install int2lm@c2sm_master%pgi')
+        if machine == 'tsa':
+            run('spack install int2lm@c2sm_master%pgi')
 
     # def test_install_test(self):
     #     # TODO: Decide if we want to integrate this test or not. It has been used lately here: From https://github.com/C2SM/spack-c2sm/pull/319
@@ -184,7 +185,8 @@ class Int2lmTest(unittest.TestCase):
 
     def test_install_no_pollen(self):
         # So our quick start tutorial works: https://c2sm.github.io/spack-c2sm/QuickStart.html
-        run('spack install int2lm@org_master%pgi pollen=False')
+        if machine == 'tsa':
+            run('spack install int2lm@org_master%pgi pollen=False')
 
 
 class IconDuskE2ETest(unittest.TestCase):
