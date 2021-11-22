3553a799066b3c4e92fe331e20c943e8c7d378ee
Dominic Hofer <dohofer86@gmail.com>
2021-11-22 18:03:23 +0100

    Improves testing and logging.

--- test_spack.py
+++ test_spack.py
@@ -330,8 +330,18 @@ def Self_and_up(origin: set, DAG: map) -> set:
             return reachers
 
 
-def Has_cycle(directed_graph: map) -> bool:
-    return False  # TODO: Implement!
+def Has_cycle(directed_graph: map, visited=None, vertex=None) -> bool:
+    if visited is None and vertex is None:
+        return any(Has_cycle(directed_graph, set(), element) for element in directed_graph)
+
+    if vertex not in directed_graph:
+        return False
+    for next in directed_graph[vertex]:
+        if next in visited:
+            return True
+        if Has_cycle(directed_graph, visited | set(next), next):
+            return True
+    return False
 
 
 class DAG_Algorithm_Test(unittest.TestCase):
@@ -347,9 +357,9 @@ class DAG_Algorithm_Test(unittest.TestCase):
         reachers = Self_and_up({'c'}, DAG)
         self.assertEqual(reachers, {'a', 'b', 'c'})
 
-    def test_diamon(self):
+    def test_diamond(self):
         # a -> b+c -> d
-        DAG = {'a': {'b', 'c'}, 'b': {'d'}, 'c': {'d'}}
+        DAG = {'a': {'b', 'c'}, 'b': {'d'}, 'c': {'d'}, 'd': {}}
         b_reachers = Self_and_up({'b'}, DAG)
         c_reachers = Self_and_up({'c'}, DAG)
         d_reachers = Self_and_up({'d'}, DAG)
@@ -357,19 +367,19 @@ class DAG_Algorithm_Test(unittest.TestCase):
         self.assertEqual(c_reachers, {'a', 'c'})
         self.assertEqual(d_reachers, {'a', 'b', 'c', 'd'})
 
-    # def test_cycle(self): TODO: Uncomment once Has_cycle is implemented!
-    #     # a <-> b
-    #     graph = {'a': {'b'}, 'b': {'a'}}
-    #     self.assertTrue(Has_cycle(graph))
+    def test_cycle(self):
+        # a <-> b
+        graph = {'a': {'b'}, 'b': {'a'}}
+        self.assertTrue(Has_cycle(graph))
 
     def test_no_cycle(self):
         # a -> b
-        graph = {'a': {'b'}}
+        graph = {'a': {'b'}, 'b': {}}
         self.assertFalse(Has_cycle(graph))
 
-    def test_diamon_is_acyclic(self):
+    def test_diamond_is_acyclic(self):
         # a -> b+c -> d
-        DAG = {'a': {'b', 'c'}, 'b': {'d'}, 'c': {'d'}}
+        DAG = {'a': {'b', 'c'}, 'b': {'d'}, 'c': {'d'}, 'd': {}}
         self.assertFalse(Has_cycle(DAG))
 
 
@@ -383,7 +393,7 @@ class SelfTest(unittest.TestCase):
         self.assertFalse(Has_cycle(dependencies))
 
     def test_expansions_is_acyclic(self):
-        self.assertFalse(Has_cycle(expansions))
+        self.assertFalse(Has_cycle(dependencies | expansions))
 
     def test_all_dependencies_are_packages(self):
         all_package_names = {case.package_name for case in all_test_cases}
@@ -395,7 +405,8 @@ class SelfTest(unittest.TestCase):
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
 
-    # Do self test first to fail fast
+    # Do self-test first to fail fast
+    print('Self-tests:')
     suite = unittest.TestSuite([
         test_loader.loadTestsFromTestCase(DAG_Algorithm_Test),
         test_loader.loadTestsFromTestCase(SelfTest)
@@ -423,6 +434,7 @@ if __name__ == '__main__':
         commands.remove('--tsa')
 
     # configure spack
+    print(f'Configuring spack with upstream {upstream} on machine {machine}.')
     subprocess.run(
         f'python ./config.py -m {machine} -i . -r ./spack/etc/spack -p ./spack -s ./spack -u {upstream} -c ./spack-cache',
         check=True,
@@ -432,8 +444,9 @@ if __name__ == '__main__':
 
     # handles backward compatibility to run any command
     if any(c not in known_commands for c in commands):
-        print('Input contains unknown command.')
-        run(' '.join(commands))
+        joined_command = ' '.join(commands)
+        print(f'Input contains unknown command. Executing: {joined_command}')
+        run(joined_command)
         sys.exit()
 
     commands = set(commands)
@@ -457,5 +470,6 @@ if __name__ == '__main__':
     ])
 
     # run tests
+    print(f'Testing packages: {packages_to_test}')
     result = unittest.TextTestRunner(verbosity=2).run(suite)
     sys.exit(not result.wasSuccessful())
