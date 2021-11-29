9f8383f4f3bf91df30990eb7f2e622a99af0a786
Dominic Hofer <dohofer86@gmail.com>
2021-11-29 14:39:55 +0100

    Adds new version of cosmo-eccodes-definitions

--- packages/cosmo-eccodes-definitions/package.py
+++ packages/cosmo-eccodes-definitions/package.py
@@ -32,6 +32,7 @@ class CosmoEccodesDefinitions(Package):
 
     maintainers = ['egermann']
 
+    version('2.19.0.6', tag='v2.19.0.6')
     version('2.19.0.5', tag='v2.19.0.5')
     version('2.19.0.4', commit='6907d3e70cff08b17e66db15f717e4bf914be5ea')
     version('2.19.0.3', commit='35640b15d5cab1b088982ca5f322a7de366cb240')
