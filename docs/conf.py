10c13e5ac54e1eb43448cec96630fd8007656e2e
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2021-12-16 16:08:41 +0100

    DOC: undo, back to p-green

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
-# html_theme_path, html_theme, needs_sphinx = set_psphinxtheme('p-green')
+#html_theme = 'groundwork'
+html_theme_path, html_theme, needs_sphinx = set_psphinxtheme('p-green')
 #html_theme = 'p-main_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
