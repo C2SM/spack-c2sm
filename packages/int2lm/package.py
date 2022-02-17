148cc1c0afe4c19e8ac96c2592d05d6f650d2aeb
Jonas Jucker <joni.jucker@yahoo.de>
2022-02-17 16:40:29 +0100

    correct typo

--- packages/int2lm/package.py
+++ packages/int2lm/package.py
@@ -75,7 +75,7 @@ class Int2lm(MakefilePackage):
 
     def setup_run_environment(self, env):
         # Account for a wrong path in nvidia-module on Daint
-        if self.spec['slave'].value == 'daint' and self.compiler.name == 'nvhpc':
+        if self.spec.variants['slave'].value == 'daint' and self.compiler.name == 'nvhpc':
             env.prepend_path('LD_LIBRARY_PATH','/opt/nvidia/hpc_sdk/Linux_x86_64/21.3/compilers/lib/')
 
     def setup_build_environment(self, env):
