ae8a8d90f224fa8afbc808d4814a840cf7a273e8
Elsa Germann <elsa.germann@meteoswiss.ch>
2022-02-17 22:02:04 +0100

    RM: obligatory %gcc by claw

--- packages/claw/package.py
+++ packages/claw/package.py
@@ -42,13 +42,13 @@ class Claw(CMakePackage):
             commit='16b165a443b11b025a77cad830b1280b8c9bcf01',
             submodules=True)
 
-    depends_on('cmake@3.12:%gcc', type='build')
+    depends_on('cmake%gcc', type='build')
     depends_on('java@8:', when="@2.0:")
     depends_on('java@7:', when="@1.1.0:1.2.3")
-    depends_on('ant@1.9:%gcc', type='build')
-    depends_on('libxml2%gcc')
-    depends_on('bison%gcc', type='build')
-    depends_on('flex%gcc', type='build')
+    depends_on('ant@1.9:', type='build')
+    depends_on('libxml2')
+    depends_on('bison', type='build')
+    depends_on('flex', type='build')
 
     def setup_environment(self, spack_env, run_env):
         spack_env.set('YACC', 'bison -y')
