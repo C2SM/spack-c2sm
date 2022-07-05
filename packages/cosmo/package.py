881f397dc0d3d094480fe044e77806c52473123d
Jonas Jucker <joni.jucker@yahoo.de>
2022-07-05 10:07:59 +0200

    make cmake compiler agnostic

--- packages/cosmo/package.py
+++ packages/cosmo/package.py
@@ -63,7 +63,7 @@ class Cosmo(MakefilePackage):
     depends_on('omni-xmod-pool', when='+claw', type='build')
     depends_on('claw%gcc', when='+claw', type='build')
     depends_on('boost%gcc', when='cosmo_target=gpu ~cppdycore', type='build')
-    depends_on('cmake%gcc', type='build')
+    depends_on('cmake', type='build')
     depends_on('zlib_ng +compat', when='+zlib_ng', type=('link', 'run'))
     depends_on('oasis', when='+oasis', type=('build', 'link', 'run'))
 
