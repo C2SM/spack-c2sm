7c8a7f84236bd8f79140857aa05fe2106c804283
Dominic Hofer <dohofer86@gmail.com>
2022-08-24 17:58:25 +0200

    Removes compilers from 'depends_on(..)'.

--- packages/icon/package.py
+++ packages/icon/package.py
@@ -50,7 +50,7 @@ class Icon(Package):
     version('2.0.17', commit='39ed04ad', submodules=True)
 
     depends_on('cmake')
-    depends_on('libxml2@2.9.8:%gcc', type=('build', 'link', 'run'))
+    depends_on('libxml2@2.9.8:', type=('build', 'link', 'run'))
     depends_on('eccodes@2.19.0 +build_shared_libs',
                when='+eccodes',
                type=('build', 'link', 'run'))
