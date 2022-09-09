b66add78580b2be3ff994c3d324fa8b4d38e7a9f
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-09-09 15:32:14 +0200

    fdb

--- packages/fdb-fortran/package.py
+++ packages/fdb-fortran/package.py
@@ -18,7 +18,7 @@ class FdbFortran(CMakePackage):
 
     depends_on('cmake@3.10:', type=('build'))
     depends_on(
-        'fdb@5.10.8 ^eckit@1.20.2: ^eccodes@2.19.0 +build_shared_libs jp2k=jasper ^metkit@1.9.2',
+        'fdb@5.10.8 ^eckit@1.20.2: ^eccodes@2.19.0 +build_shared_libs ^metkit@1.9.2',
         type=('build', 'link'))
 
 
--- packages/flexpart-ifs/package.py
+++ packages/flexpart-ifs/package.py
@@ -17,8 +17,14 @@ class FlexpartIfs(MakefilePackage):
             git='git@github.com:MeteoSwiss-APN/flexpart-ifs.git',
             branch='meteoswiss-10')
 
+    version('fdb',
+            git='git@github.com:MeteoSwiss-APN/fdb-flexpart.git',
+            branch='fdb')
+
     depends_on('eccodes jp2k=none +fortran', type=('build', 'link'))
     depends_on('netcdf-fortran', type=('build', 'link'))
+    depends_on('fdb-fortran@archive_retreive',when='@fdb', type=('build', 'link'))
+    depends_on('fdb@5.10.8 ^eckit@1.20.2: ~mpi ^eccodes@2.19.0 +build_shared_libs ^metkit@1.9.2', when='@fdb', type=('build', 'link'))
     build_directory = 'src'
 
     @property
@@ -31,14 +37,21 @@ class FlexpartIfs(MakefilePackage):
 
     def setup_build_environment(self, env):
         env.set('ECCODESROOT', self.spec['eccodes'].prefix)
-        env.set(
-            'ECCODES_LD_FLAGS', '-L' + self.spec['eccodes'].prefix +
-            '/lib64 -leccodes_f90 -leccodes')
         env.set('EBROOTNETCDFMINFORTRAN', self.spec['netcdf-fortran'].prefix)
-        #abuse of JASPER_LD_FLAGS since there is no other entrypoint var for LDFLAGS
-        env.set('JASPER_LD_FLAGS', '-Wl,--no-relax')
-        # not really required, just a default since the -I flags would be inconsistent with an empty string
-        env.set('CURL_INCLUDES', '/usr')
+        env.set(
+                'ECCODES_LD_FLAGS', '-L' + self.spec['eccodes'].prefix +
+                '/lib64 -leccodes_f90 -leccodes')
+        if self.spec.version not in (Version('fdb')):
+            env.set('JASPER_LD_FLAGS', '-Wl,--no-relax')
+            # not really required, just a default since the -I flags would be inconsistent with an empty string
+            env.set('CURL_INCLUDES', '/usr')
+        else:
+            env.set('JASPER_LD_FLAGS', '-Wl,--no-relax' +' -L' + self.spec['fdb'].prefix +
+                '/lib'  + ' -lfdb5' + ' -L' + self.spec['fdb-fortran'].prefix + '/lib' + ' -lfdbf')
+            # not really required, just a default since the -I flags would be inconsistent with an empty string
+            env.set('CURL_INCLUDES', '/usr' 
+                + ' -I' + self.spec['fdb-fortran'].prefix + '/include/fortran/fdb/modules'
+                + ' -I' + self.spec['fdb'].prefix + '/include')
 
     def install(self, spec, prefix):
         mkdir(prefix.bin)
