e39f0845cab8e30609a41b4cb970ca1843441754
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-09-13 11:59:48 +0200

    eccodes dependencies in flexpart@fdb

--- packages/flexpart-ifs/package.py
+++ packages/flexpart-ifs/package.py
@@ -21,15 +21,10 @@ class FlexpartIfs(MakefilePackage):
             git='git@github.com:MeteoSwiss-APN/fdb-flexpart.git',
             branch='fdb')
 
-    depends_on('eccodes jp2k=none +fortran', type=('build', 'link'))
+    depends_on('eccodes jp2k=none +fortran', when='@meteoswiss-10', type=('build', 'link'))
     depends_on('netcdf-fortran', type=('build', 'link'))
-    depends_on('fdb-fortran@archive_retreive',
-               when='@fdb',
-               type=('build', 'link'))
-    depends_on(
-        'fdb@5.10.8 ^eckit@1.20.2: ~mpi ^eccodes@2.19.0 +build_shared_libs ^metkit@1.9.2',
-        when='@fdb',
-        type=('build', 'link'))
+    depends_on('fdb-fortran@archive_retreive',when='@fdb', type=('build', 'link'))
+    depends_on('fdb@5.10.8 ^eckit@1.20.2: ~mpi ^eccodes@2.19.0 jp2k=none +fortran +build_shared_libs ^metkit@1.9.2', when='@fdb', type=('build', 'link'))
     build_directory = 'src'
 
     @property
