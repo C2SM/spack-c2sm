d38c45c21ee91a08d71cd2bf5927301ae34bd566
Dominic Hofer <dohofer86@gmail.com>
2022-10-04 17:02:14 +0200

    Adds 'status.sh'

---
+++ status.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo ">>> GIT STATUS <<<"
+git status
+echo ""
+echo ">>> SPACK --VERSION <<<"
+spack --version
+echo ""
+echo ">>> SPACK_SYSTEM_CONFIG_PATH <<<"
+echo $SPACK_SYSTEM_CONFIG_PATH
+echo ""
+echo ">>> MACHINE <<<"
+env-setup/machine.sh
+echo ""
+echo ""
+echo ">>> PYTHON --VERSION <<<"
+python --version
+echo ""
+echo ">>> PYTHON3 --VERSION <<<"
+python3 --version
