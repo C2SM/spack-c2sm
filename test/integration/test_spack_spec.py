daf68580047237b81d4d83cc5ff1dfbc1efbc3b2
Dominic Hofer <dohofer86@gmail.com>
2022-10-04 17:01:40 +0200

    Adds tests

---
+++ test/integration/test_spack_spec.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Tests that the command 'SPACK SPEC ...' work.
+
+import os
+import unittest
+
+repo_path = os.path.dirname(os.path.realpath(__file__)) + '/../..'
+
+
+class TestCosmo(unittest.TestCase):
+
+    def test_name(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/unit/README.md
@@ -0,0 +1,2 @@
+# Unit tests
+These tests are designed to run without a spack instance. They do not test spack. They do not test packages. They only test package descriptions or other stand-alone components.
