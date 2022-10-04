9b4da73681ad4ce8c0d12ab0fac74312630f4f3c
Dominic Hofer <dohofer86@gmail.com>
2022-10-04 19:08:34 +0200

    Improves integration tests

--- test/integration/test_spack_spec.py
+++ test/integration/test_spack_spec.py
@@ -2,7 +2,6 @@
 
 # Tests that the command 'SPACK SPEC ...' work.
 
-import os
 import subprocess
 import unittest
 
@@ -10,19 +9,19 @@ import unittest
 class CosmoTest(unittest.TestCase):
 
     def test_org_master(self):
-        subprocess.run(f'spack spec cosmo @org-master', check=True, shell=True)
+        subprocess.run('spack spec cosmo @org-master', check=True, shell=True)
 
 
 class IconTest(unittest.TestCase):
 
     def test_nwp(self):
-        subprocess.run(f'spack spec icon @nwp', check=True, shell=True)
+        subprocess.run('spack spec icon @nwp', check=True, shell=True)
 
 
 class Int2lmTest(unittest.TestCase):
 
     def test_org_master(self):
-        subprocess.run(f'spack spec int2lm @org-master',
+        subprocess.run('spack spec int2lm @org-master',
                        check=True,
                        shell=True)
 
