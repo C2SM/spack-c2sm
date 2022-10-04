356087aa110d965f5020e0b155676006aab1644c
Dominic Hofer <dohofer86@gmail.com>
2022-10-04 17:01:06 +0200

    Improves setup-env

--- setup-env.sh
+++ setup-env.sh
@@ -2,14 +2,11 @@
 
 parent_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" == 1 ]; then
     machine="$1"
 else
-    machine="$( . "$parent_dir"/env-setup/machine.sh )"
+    machine="$( "$parent_dir"/env-setup/machine.sh )"
 fi
-
-#. "$parent_dir"/env-setup/load_python.sh
-#python "$parent_dir"/install_sysconfig.py
 
 export SPACK_SYSTEM_CONFIG_PATH="$parent_dir"/sysconfigs/"$machine"
 . "$parent_dir"/spack/share/spack/setup-env.sh
