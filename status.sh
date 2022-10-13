cc48dc61be3da9e1e86a6cfb2f31fa560c956563
PanicSheep <dohofer86@gmail.com>
2022-10-13 11:59:04 +0200

    Adds script to print status

---
+++ status.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+parent_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+echo ">>> GIT --VERSION <<<"
+git --version
+echo ""
+echo ">>> GIT STATUS <<<"
+git -C "$parent_dir" status
+echo ""
+echo ">>> PYTHON --VERSION <<<"
+python --version
+echo ""
+echo ">>> PYTHON3 --VERSION <<<"
+python3 --version
+echo ""
+echo ">>> SPACK --VERSION <<<"
+spack --version
+echo ""
+echo ">>> SPACK_SYSTEM_CONFIG_PATH <<<"
+echo $SPACK_SYSTEM_CONFIG_PATH
+echo ""
+echo ">>> MACHINE <<<"
+"$parent_dir"/env-setup/machine.sh
+echo ""
