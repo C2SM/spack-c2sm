d3b389bfeb299033c561676a5b9c2057c94f7521
PanicSheep <dohofer86@gmail.com>
2022-10-13 12:03:50 +0200

    Removes version detection

--- packages/int2lm/package.py
+++ packages/int2lm/package.py
@@ -8,7 +8,6 @@ from spack import *
 
 import os
 import subprocess
-from version_detection import set_versions
 
 
 class Int2lm(MakefilePackage):
@@ -27,16 +26,13 @@ class Int2lm(MakefilePackage):
 
     # APN tags
     version('apn-master', git=git, branch='master')
-    set_versions(version, git, 'apn')
 
     # C2SM tags
     version('c2sm-master', git=c2smgit, branch='master')
     version('c2sm-features', git=c2smgit, branch='c2sm-features')
-    set_versions(version, c2smgit, 'c2sm')
 
     # ORG tags
     version('org-master', git=orggit, branch='master')
-    set_versions(version, orggit, 'org')
 
     # deprecated versions
     version('apn_master', git=git, branch='master')
