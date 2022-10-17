f168c5411d58bce8ba85595ae9a0e7e19e24588d
Jonas Jucker <joni.jucker@yahoo.de>
2022-10-17 15:48:18 +0200

    create links to logfiles

--- send_summary_as_comment_to_PR.py
+++ send_summary_as_comment_to_PR.py
@@ -1,11 +1,13 @@
 import glob
 import subprocess
 import os
+import sys
 
 def collect_final_status_from_logs():
     failed = []
     write_locked = []
     passed = []
+
     for log in glob.glob('*.log'):
         with open(log,'r'):
             if search_str_in_file(log,'FAIL: SPACK'):
@@ -31,18 +33,17 @@ def search_str_in_file(file_path, word):
         else:
             return False
 
-def compose_markdown_comment(status):
-    comment = '###Passed### \\n'
-    for test in status['passed']:
-        comment = f'{comment} - {test}: **PASSED** \\n'
+def compose_markdown_comment(machine,status,links):
+    comment = '## ' + machine.upper() + ' \\n'
+    comment += '  ### Passed \\n'
+    for test,link in zip(status['passed'],links['passed']):
+        comment = f'{comment} - [{test}]({link}): **PASSED** \\n'
 
-    comment = f'{comment} ###Failed### \\n'
-    for test in status['failed']:
-        comment = f'{comment} - {test}: **FAILED** \\n'
-
-    comment = f'{comment} ###Write lock timeout### \\n'
-    for test in status['write_locked']:
-        comment = f'{comment} - {test}: **WRITE LOCK TIMEOUT** \\n'
+    comment = f'{comment}  ### Failed \\n'
+    for test,link in zip(status['failed'],links['failed']):
+        comment = f'{comment} - [{test}]({link}): **FAILED** \\n'
+    for test,link in zip(status['write_locked'],links['write_locked']):
+        comment = f'{comment} - [{test}]({link}): **WRITE LOCK TIMEOUT** \\n'
 
     return comment
 
@@ -53,13 +54,41 @@ def post_on_PR(comment):
     command += '-X POST ' 
     command += '-d "{\\"body\\":\\"${COMMENT_MARKDOWN}\\"}" '
     command += '"https://api.github.com/repos/c2sm/spack-c2sm/issues/${ghprbPullId}/comments" '
-    print(command)
     subprocess.run(command,
         check=True,
         shell=True)
 
+def compose_links_to_artifacts(status):
+
+    base_link = 'https://jenkins-mch.cscs.ch/job/spack_PR'
+    pr_id = os.getenv('ghprbPullId')
+    pr_link = f'{base_link}/{pr_id}/artifact'
+    
+    failed = []
+    write_locked = []
+    passed = []
+    for test in status['passed']:
+        passed.append(f'{pr_link}/{test}')
+
+    for test in status['failed']:
+        failed.append(f'{pr_link}/{test}')
+
+    for test in status['write_locked']:
+        write_locked.append(f'{pr_link}/{test}')
+
+    links = {}
+    links['failed'] = failed
+    links['write_locked'] = write_locked
+    links['passed'] = passed
+
+    return links
+
 if __name__ == '__main__':
 
+    machine = sys.argv[1]
+
     status = collect_final_status_from_logs()
-    comment = compose_markdown_comment(status)
+    links = compose_links_to_artifacts(status)
+    comment = compose_markdown_comment(machine,status,links)
+    print(comment)
     post_on_PR(comment)
