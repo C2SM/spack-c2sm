f45b3e130fa56137206d465dad644b5fa1815cd3
Dominic Hofer <dohofer86@gmail.com>
2022-10-26 15:27:06 +0200

    Adds test for dev-build icon22_benchmark_4. Comments out broken tests.

--- test/system_test.py
+++ test/system_test.py
@@ -17,17 +17,31 @@ def spack_install(package):
 
 class IconTest(unittest.TestCase):
 
-    def test_install_nwp_cpu(self):
-        spack_install('icon @nwp icon_target=cpu')
+    def test_dev_build_benchmark_4_cpu(self):
+        subprocess.run('git clone -branch icon22_benchmark_4 --recursive ssh://git@gitlab.dkrz.de/icon/icon-nwp.git', check=True, shell=True)
+        subprocess.run('mkdir -p icon-nwp/cpu', check=True, shell=True)
+        subprocess.run('touch .dummy_file', cwd='icon-nwp/cpu', check=True, shell=True)
+        subprocess.run(f'. {spack_c2sm_path}/setup-env.sh; spack dev-build -u build icon @develop config_dir=./.. icon_target=cpu', cwd='icon-nwp/cpu', check=True, shell=True)
+        subprocess.run('rm -rf icon-nwp', check=True, shell=True)
 
-    def test_install_nwp_gpu(self):
-        spack_install('icon @nwp icon_target=gpu')
+    def test_dev_build_benchmark_4_gpu(self):
+        subprocess.run('git clone -branch icon22_benchmark_4 --recursive ssh://git@gitlab.dkrz.de/icon/icon-nwp.git', check=True, shell=True)
+        subprocess.run('mkdir -p icon-nwp/gpu', check=True, shell=True)
+        subprocess.run('touch .dummy_file', cwd='icon-nwp/gpu', check=True, shell=True)
+        subprocess.run(f'. {spack_c2sm_path}/setup-env.sh; spack dev-build -u build icon @develop config_dir=./.. icon_target=gpu', cwd='icon-nwp/gpu', check=True, shell=True)
+        subprocess.run('rm -rf icon-nwp', check=True, shell=True)
 
-    def test_install_nwp_all_deps(self):
-        "Triggers conditional dependencies"
-        spack_install(
-            '--test=root icon @nwp icon_target=gpu serialize_mode=create +eccodes +claw'
-        )
+    # def test_install_nwp_cpu(self):
+    #     spack_install('icon @nwp icon_target=cpu')
+
+    # def test_install_nwp_gpu(self):
+    #     spack_install('icon @nwp icon_target=gpu')
+
+    # def test_install_nwp_all_deps(self):
+    #     "Triggers conditional dependencies"
+    #     spack_install(
+    #         '--test=root icon @nwp icon_target=gpu serialize_mode=create +eccodes +claw'
+    #     )
 
 
 if __name__ == '__main__':
