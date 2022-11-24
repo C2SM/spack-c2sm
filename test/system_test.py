27c042db3f992f924c89af88b72b7046bc6c3da1
Dominic Hofer <dominic.hofer@meteoswiss.ch>
2022-11-24 17:34:12 +0100

    Improves system tests

--- test/system_test.py
+++ test/system_test.py
@@ -20,58 +20,136 @@ def test_with_spack(command: str, log_name: str = None):
     ret.check_returncode()
 
 
-def spack_install(command: str, log_name: str = None):
-    test_with_spack(f'spack install {command}', log_name)
+def spack_install_and_test(command: str, log_name: str = None):
+    if 'cosmo' in command and 'cosmo-dycore' not in command:
+        test_with_spack(f'spack installcosmo -v {command}', log_name)
+    else:
+        test_with_spack(f'spack install --show-log-on-error --test=root {command}', log_name)
 
 
+def needs_testing(package: str) -> bool:
+    if 'jenkins' in sys.argv:
+        return package in sys.argv or 'all' in sys.argv
+    return True
+
+
+@unittest.skipUnless(needs_testing('cosmo'), 'irrelevant')
 class CosmoTest(unittest.TestCase):
+    package_name = 'cosmo'
 
     def test_install_version_6_0(self):
-        spack_install('--test=root cosmo @6.0')
+        spack_install_and_test('cosmo @6.0')
 
     def test_install_version_5_09_mch_1_2_p2(self):
-        spack_install('--test=root cosmo @5.09a.mch1.2.p2')
+        spack_install_and_test('cosmo @5.09a.mch1.2.p2')
 
 
+@unittest.skipUnless(needs_testing('cosmo-dycore'), 'irrelevant')
+class CosmoDycoreTest(unittest.TestCase):
+    package_name = 'cosmo-dycore'
+
+    def test_install_version_6_0(self):
+        spack_install_and_test('cosmo-dycore @6.0')
+
+    def test_install_version_6_0_no_cuda(self):
+        spack_install_and_test('cosmo-dycore @6.0 ~cuda')
+
+    def test_install_version_5_09_mch_1_2_p2(self):
+        spack_install_and_test('cosmo-dycore @5.09a.mch1.2.p2')
+
+
+class CosmoEccodesDefinitionsTest(unittest.TestCase):
+    package_name = 'cosmo-eccodes-definitions'
+
+
+class CosmoGribApiTest(unittest.TestCase):
+    package_name = 'cosmo-grib-api'
+
+
+class CosmoGribApiDefinitionsTest(unittest.TestCase):
+    package_name = 'cosmo-grib-api-definitions'
+    
+
+class DawnTest(unittest.TestCase):
+    package_name = 'dawn'
+    
+
+class Dawn4PyTest(unittest.TestCase):
+    package_name = 'dawn4py'
+
+
+class DuskTest(unittest.TestCase):
+    package_name = 'dusk'
+
+
+class GridToolsTest(unittest.TestCase):
+    package_name = 'gridtools'
+
+
+@unittest.skipUnless(needs_testing('icon'), 'irrelevant')
+@unittest.skipIf(machine_name() in ['tsa', 'manali'], 'config file does not exist for these machines')
 class IconTest(unittest.TestCase):
-
-    def setUp(self):
-        if machine_name() in ['tsa']:
-            self.skipTest()
+    package_name = 'icon'
 
     def test_install_version_2_6_5(self):
-        spack_install('--test=root icon @2.6.5')
+        spack_install_and_test('icon @2.6.5')
 
     def test_install_nwp_cpu(self):
-        spack_install('icon @nwp %nvhpc icon_target=cpu')
+        spack_install_and_test('icon @nwp %nvhpc icon_target=cpu')
 
     def test_install_nwp_gpu(self):
-        spack_install('icon @nwp icon_target=gpu')
+        spack_install_and_test('icon @nwp icon_target=gpu')
 
     def test_install_nwp_all_deps(self):
         """Triggers conditional dependencies"""
 
-        spack_install(
-            '--test=root icon @nwp icon_target=cpu serialize_mode=create +eccodes +claw'
-        )
+        spack_install_and_test('icon @nwp icon_target=gpu serialize_mode=create +eccodes +claw')
 
 
+@unittest.skipUnless(needs_testing('int2lm'), 'irrelevant')
 class Int2lmTest(unittest.TestCase):
+    package_name = 'int2lm'
 
     def test_install_gcc(self):
-        spack_install('int2lm @c2sm-master %gcc')
+        spack_install_and_test('int2lm @c2sm-master %gcc')
 
     def test_install_nvhpc(self):
-        if machine_name() in ['tsa']:
-            return
-        spack_install('int2lm @c2sm-master %nvhpc')
+        spack_install_and_test('int2lm @c2sm-master %nvhpc')
 
 
+@unittest.skipUnless(needs_testing('icontools'), 'irrelevant')
 class IconToolsTest(unittest.TestCase):
+    package_name = 'icontools'
 
     def test_install(self):
-        spack_install('icontools @c2sm-master %gcc')
+        spack_install_and_test('icontools @c2sm-master %gcc')
+
+
+class LibGrib1Test(unittest.TestCase):
+    package_name = 'libgrib1'
+
+
+class OasisTest(unittest.TestCase):
+    package_name = 'oasis'
+
+
+class OmniXmodPoolTest(unittest.TestCase):
+    package_name = 'omni-xmod-pool'
+
+
+class OmniCompilerTest(unittest.TestCase):
+    package_name = 'omnicompiler'
+
+
+class XcodeMLToolsTest(unittest.TestCase):
+    package_name = 'xcodeml-tools'
+
+
+class ZLibNGTest(unittest.TestCase):
+    package_name = 'zlib_ng'
 
 
 if __name__ == '__main__':
+    commands = sys.argv[1:]
+    sys.argv = [sys.argv[0]]  # unittest needs this
     unittest.main(verbosity=2)
