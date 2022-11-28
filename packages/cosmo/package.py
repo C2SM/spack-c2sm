35bd8c8265960bcaf9ab23efb2d490138b2455d5
Dominic Hofer <dominic.hofer@meteoswiss.ch>
2022-11-28 18:15:34 +0100

    [cosmo] Removes cuda_arch 80.

--- packages/cosmo/package.py
+++ packages/cosmo/package.py
@@ -116,7 +116,7 @@ class Cosmo(MakefilePackage):
     variant('cuda_arch',
             default='none',
             description='Build with cuda_arch',
-            values=('80', '70', '60', '37'),
+            values=('70', '60', '37'),
             multi=False)
     variant(
         'zlib_ng',
