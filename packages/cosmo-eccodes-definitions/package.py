295ab8a129802d47c8d027defa4cac9022e5de74
Jonas Jucker <joni.jucker@yahoo.de>
2022-12-01 09:41:24 +0100

    add eccodes-deps for cosmo-eccodes-definitions again

--- packages/cosmo-eccodes-definitions/package.py
+++ packages/cosmo-eccodes-definitions/package.py
@@ -43,7 +43,16 @@ class CosmoEccodesDefinitions(Package):
     version('2.14.1.2', tag='v2.14.1.2')
     version('2.14.1.1', tag='v2.14.1.1')
 
-    depends_on('eccodes +fortran')
+    depends_on('eccodes')
+    depends_on('eccodes@2.19.0',
+               type=('build', 'link', 'run'),
+               when='@2.19.0.1:')
+    depends_on('eccodes@2.18.0',
+               type=('build', 'link', 'run'),
+               when='@2.18.0.1')
+    depends_on('eccodes@2.14.1',
+               type=('build', 'link', 'run'),
+               when='@2.14.1.1:2.14.1.2')
 
     def setup_run_environment(self, env):
         eccodes_definition_path = self.prefix + '/cosmoDefinitions/definitions/:' + self.spec[
