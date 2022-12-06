08550a2b1eb8c3c61f32462009e086891e8ef5cb
Jonas Jucker <joni.jucker@yahoo.de>
2022-12-06 15:09:34 +0100

    some unittests pass for icon4py

--- packages/py-gt4py/package.py
+++ packages/py-gt4py/package.py
@@ -11,21 +11,19 @@
 # next to all the things you'll want to change. Once you've handled
 # them, you can save this file and test your package like this:
 #
-#     spack install py-icon4py
+#     spack install py-gt4py
 #
 # You can edit this file again by typing:
 #
-#     spack edit py-icon4py
+#     spack edit py-gt4py
 #
 # See the Spack documentation for more information on packaging.
 # ----------------------------------------------------------------------------
 
 from spack import *
-import os
-import inspect
 
 
-class PyIcon4py(PythonPackage):
+class PyGt4py(PythonPackage):
     """FIXME: Put a proper description of your package here."""
 
     # FIXME: Add a proper url for your package's homepage here.
@@ -33,29 +31,32 @@ class PyIcon4py(PythonPackage):
 
     # FIXME: ensure the package is not available through PyPI. If it is,
     # re-run `spack create --force` with the PyPI URL.
-    url      = "git@github.com:C2SM/icon4py.git"
-#    build_directory = 'atm_dyn_iconam'
+    url      = "ssh://git@github.com/GridTools/gt4py.git"
+
+    version('functional',
+            branch='functional',
+            git=url)
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
     # maintainers = ['github_user1', 'github_user2']
 
     # FIXME: Add proper versions here.
-    version('main',
-            branch='main',
-            git='ssh://git@github.com/C2SM/icon4py.git')
+    # version('1.2.4')
 
     # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
     # or need to change the dependency type. Generic python/pip/wheel dependencies are
     # added implicity by the PythonPackage base class.
     depends_on('python@3.10:', type=('build', 'run'))
+    # depends_on('py-pip@X.Y:', type='build')
     depends_on('py-wheel', type='build')
-    depends_on('py-pytest', type='test')
+    depends_on('py-cython', type='build')
 
     # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
     # exists, use setuptools.
     depends_on('py-setuptools', type='build')
-    depends_on('py-gt4py', type=('build','run'))
+    # depends_on('py-flit-core', type='build')
+    # depends_on('py-poetry-core', type='build')
 
     # FIXME: Add additional dependencies if required.
     # depends_on('py-foo', type=('build', 'run'))
@@ -71,31 +72,3 @@ class PyIcon4py(PythonPackage):
         # FIXME: If not needed, delete this function
         options = []
         return options
-
-    def install(self, spec, prefix):
-        """Install everything from build directory."""
-
-        args = PythonPackage._std_args(self) + ['--prefix=' + prefix]
-
-        for option in self.install_options(spec, prefix):
-            args.append('--install-option=' + option)
-        for option in self.global_options(spec, prefix):
-            args.append('--global-option=' + option)
-
-        if self.stage.archive_file and self.stage.archive_file.endswith('.whl'):
-            args.append(self.stage.archive_file)
-        else:
-            args.append('.')
-
-        pip = inspect.getmodule(self).pip
-        build_dirs = ['common','pyutils','testutils','atm_dyn_iconam']
-        for dir in build_dirs:
-            with working_dir(os.path.join(self.stage.source_path,dir)):
-                pip(*args)
-
-    @run_after('install')
-    @on_package_attributes(run_tests=True)
-    def install_test(self):
-        with working_dir('spack-test', create=True):
-            python('-m', 'pytest', '-v','../pyutils/tests')
-
