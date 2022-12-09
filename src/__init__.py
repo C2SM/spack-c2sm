06747a796ef8c083d079321d71fab6f0de50ab4f
Dominic Hofer <dohofer86@gmail.com>
2022-12-09 12:27:39 +0100

    Adds format.py and tests.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from .format import time_format, sanitized_filename
+from .github import GitHubRepo, Markdown
 from .machine import machine_name
 from .spack_commands import with_spack, log_with_spack
-from .github import GitHubRepo, Markdown
---
+++ src/format.py
@@ -0,0 +1,27 @@
+
+def time_format(seconds) -> str:
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+
+    parts = []
+    if h:
+        parts.append(f'{h:.0f}h')
+    if m:
+        parts.append(f'{m:.0f}m')
+    if s:
+        parts.append(f'{s:.2f}s')
+    return ' '.join(parts)
+
+
+def sanitized_filename(filename: str) -> str:
+    replacements = [
+        (' --show-log-on-error', ''), # irrelevant in filename
+        (' --test=root', ''), # irrelevant in filename
+        (' -n', ''), # irrelevant in filename
+        (' -v', ''), # irrelevant in filename
+        ('%', ''), # causes problems in web browsers
+        (' ', '_'), # causes problems in bash
+    ]
+    for old, new in replacements:
+        filename = filename.replace(old, new)
+    return filename
