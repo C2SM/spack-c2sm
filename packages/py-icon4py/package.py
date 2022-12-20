be5632841cacb1b27722208a52d9946e2669109e
Jonas Jucker <joni.jucker@yahoo.de>
2022-12-20 09:23:45 +0100

    cleanup py-icon4py

--- packages/py-icon4py/package.py
+++ packages/py-icon4py/package.py
@@ -3,43 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-icon4py
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-icon4py
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 import os
 import inspect
 
 
 class PyIcon4py(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """ICON4Py contains Python (GT4Py) implementations of ICON (inspired) 
+    components for weather and climate models."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
-
-    # FIXME: ensure the package is not available through PyPI. If it is,
-    # re-run `spack create --force` with the PyPI URL.
     url = "git@github.com:C2SM/icon4py.git"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
+    maintainers = ['samkellerhals']
 
-    # FIXME: Add proper versions here.
     version('main', branch='main', git='ssh://git@github.com/C2SM/icon4py.git')
 
     depends_on('py-wheel', type='build')
@@ -47,25 +23,10 @@ class PyIcon4py(PythonPackage):
 
     depends_on('python@3.10:', type=('build', 'run'))
     depends_on('py-tabulate@0.8.9:', type=('build', 'run'))
-
     # TODO: push new version to Spack official
     depends_on('py-fprettify@0.3.7:', type=('build', 'run'))
-
     depends_on('py-gt4py', type=('build', 'run'))
-
-    depends_on('py-pytest', type='test')
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
+    depends_on('py-pytest', type=('build', 'run'))
 
     def install(self, spec, prefix):
         """Install everything from build directory."""
