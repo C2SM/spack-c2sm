d75f0a9951c74ef5a7108bd71b4f3cdf83196f40
Dominic Hofer <dominic.hofer@meteoswiss.ch>
2022-12-21 11:56:46 +0100

    [test] Excludes icon@exclaim-master on balfrin.

--- test/system_test.py
+++ test/system_test.py
@@ -145,7 +145,7 @@ class GridToolsTest(unittest.TestCase):
         spack_install_and_test('gridtools @1.1.3')
 
 
-@pytest.mark.no_tsa  # config file does not exist for these machines
+@pytest.mark.no_tsa  # config file does not exist for this machines
 class IconTest(unittest.TestCase):
 
     def test_install_nwp_gpu(self):
@@ -165,14 +165,17 @@ class IconTest(unittest.TestCase):
         spack_install_and_test(
             f'icon @develop %nvhpc config_dir=./.. icon_target=cpu ^{mpi} %{nvidia_compiler}')
 
+    @pytest.mark.no_balfrin  # config file does not exist for this machines
     def test_install_exclaim_cpu(self):
         spack_install_and_test(
             f'icon @exclaim-master %nvhpc icon_target=cpu +eccodes +ocean ^{mpi} %{nvidia_compiler}')
 
+    @pytest.mark.no_balfrin  # config file does not exist for this machines
     def test_install_exclaim_cpu_gcc(self):
         spack_install_and_test(
             'icon @exclaim-master %gcc icon_target=gpu +eccodes +ocean +claw')
-
+            
+    @pytest.mark.no_balfrin  # config file does not exist for this machines
     def test_install_exclaim_gpu(self):
         spack_install_and_test(
             f'icon @exclaim-master %nvhpc icon_target=gpu +eccodes +ocean +claw ^{mpi} %{nvidia_compiler}'
