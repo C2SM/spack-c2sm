9bb1ca32a80702d92e854fa96f8c722e5b03484d
Jonas Jucker <joni.jucker@yahoo.de>
2023-01-26 16:04:20 +0100

    add packages

---
+++ packages/nvidia-lapack/package.py
@@ -0,0 +1,31 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+class NvidiaLapack(Package):
+    """The Cray Scientific Libraries package, LibSci, is a collection of
+    numerical routines optimized for best performance on Cray systems."""
+
+    has_code = False    # Skip attempts to fetch source that is not available
+
+    version("21.3")
+
+    provides("lapack")
+
+    @property
+    def libs(self):
+
+        lib = ["liblapack"]
+
+        return find_libraries(
+            lib,
+            root='/opt/nvidia/hpc_sdk/Linux_x86_64/21.3/compilers/lib',
+            shared=True,
+            recursive=True)
+
+    def install(self, spec, prefix):
+        raise InstallError(
+            self.spec.format('{name} is not installable, you need to specify '
+                             'it as an external package in packages.yaml'))
+
