696f8bc31e6a440c4a0f0d560a45dc9c9c14c7e3
Dominic Hofer <dominic.hofer@meteoswiss.ch>
2023-01-27 09:23:53 +0100

    Removes variant cosmo-eccodes-definitions

--- packages/int2lm/package.py
+++ packages/int2lm/package.py
@@ -34,9 +34,6 @@ class Int2lm(MakefilePackage):
     version('int2lm-3.00', git=orggit, tag='int2lm-3.00')
 
     depends_on('eccodes +fortran', when='+eccodes')
-    depends_on('cosmo-grib-api-definitions',
-               type=('build', 'run'),
-               when='~eccodes')
     depends_on('cosmo-eccodes-definitions',
                type=('build', 'run'),
                when='+eccodes')
