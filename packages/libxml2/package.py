8109deea9b2bb380a01e1b4f7350134ae00c35da
Dominic Hofer <dominic.hofer@meteoswiss.ch>
2023-03-03 17:59:20 +0100

    [libxml2] Adds conflict

---
+++ packages/libxml2/package.py
@@ -0,0 +1,11 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+from spack.pkg.builtin.libxml2 import Libxml2 as SpackLibxml2
+
+
+class Libxml2(SpackLibxml2):
+    conflicts('%nvhpc@21.3')
