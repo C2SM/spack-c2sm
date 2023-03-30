e0313056b6c00df1a4742812d11ec5cc047a546d
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2023-03-30 11:18:02 +0200

    Fix oasis mct (#680)

--- packages/oasis/package.py
+++ packages/oasis/package.py
@@ -7,6 +7,9 @@ from spack.build_systems.makefile import MakefilePackage
 from spack.directives import depends_on, version
 from llnl.util.filesystem import working_dir, FileFilter, install_tree
 import os
+import re
+from pathlib import Path
+from typing import Dict
 
 
 class Oasis(MakefilePackage):
@@ -23,6 +26,10 @@ class Oasis(MakefilePackage):
     depends_on('mpi', type=('build', 'link', 'run'))
     depends_on('netcdf-fortran', type=('build', 'link', 'run'))
 
+    variant('fix_mct_conflict',
+            default=False,
+            description='rename mct modules xxx as xxx_oasis to solve conflict with other mct instance at runtime')
+
     build_directory = 'util/make_dir'
 
     makefile_file = 'TopMakefileOasis3'
@@ -31,6 +38,7 @@ class Oasis(MakefilePackage):
     # to the absolute path called ARCHDIR in the Makefile)
     rel_ARCHDIR = 'spack-build'
 
+    
     def setup_build_environment(self, env):
 
         CHAN = 'MPI1'
@@ -51,12 +59,17 @@ class Oasis(MakefilePackage):
 
         FFLAGS = '-O2 {INCPSMILE} {CPPDEF}'.format(CPPDEF=CPPDEF,
                                                    INCPSMILE=INCPSMILE)
+
+        if self.compiler.name == 'gcc':
+            FFLAGS += ' -ffree-line-length-512'
+            
         env.set('F90FLAGS', FFLAGS)
         env.set('f90FLAGS', FFLAGS)
         env.set('FFLAGS', FFLAGS)
         env.set('fFLAGS', FFLAGS)
         env.set('CCFLAGS', FFLAGS)
 
+        
     def edit(self, spec, prefix):
 
         COUPLE = os.getcwd()
@@ -69,6 +82,7 @@ class Oasis(MakefilePackage):
                     COUPLE, ARCHDIR))
             makefile.filter('\$\(modifmakefile\)\s\;\s', '')
 
+        
     def patch(self):
 
         # Remove old directives for Fujitsu comilers. Already fixed in MCT [1] but not merged in OASIS yet
@@ -77,10 +91,54 @@ class Oasis(MakefilePackage):
             m_AttrVect = FileFilter('m_AttrVect.F90')
             m_AttrVect.filter('\s*\!DIR\$ COLLAPSE', '')
 
+
+    @run_before('build')
+    def fix_mct_conflict(self):
+        """Rename modules mct_xxx as mct_xxx_oasis"""
+
+        # Only do something when the fix_mct_conflict variant is set to True
+        if not self.spec.variants['fix_mct_conflict'].value:
+            return
+        
+        # Define regexps
+        re_module = re.compile(r'^(?P<indent>\s*)(?P<statement>module|end\s+module)'
+                               r'(?P<space>\s*)(?P<name>m\w*)(?P<rest>.*)$',
+                               re.IGNORECASE)
+        re_use = re.compile(r'^(?P<indent>\s*)use(?P<space>\s*)'
+                            r'(?P<name>m_\w*)(?P<rest>.*)$',
+                            re.IGNORECASE)
+        re_mct_mod = re.compile(r'^(?P<begining>.*)mct_mod(?P<end>.*)$', re.IGNORECASE)
+
+        # File modification function
+        def mod_file(file:Path, regex_subs:Dict, indent=3) -> None:
+
+            print(' '*indent + file.name)
+            data = file.read_text()
+            data_oasis = []
+            for line in data.splitlines():
+                for regex, sub_str in regex_subs.items():
+                    line = regex.sub(sub_str, line)
+                data_oasis.append(line)
+            file.write_text('\n'.join(data_oasis)+'\n')
+
+        # Modify mct and mpeu source files
+        for direc in 'mct', 'mpeu':
+            print(direc)
+            for src_file in Path(f'lib/mct/{direc}').glob('*90'):
+                mod_file(src_file, {re_module: r'\g<indent>\g<statement>\g<space>\g<name>_oasis\g<rest>',
+                                    re_use: r'\g<indent>use\g<space>\g<name>_oasis\g<rest>'})
+
+        # Modify psmile source files
+        print('psmile')
+        for src_file in Path(f'lib/psmile/src').glob('*90'):
+            mod_file(src_file, {re_mct_mod: r'\g<begining>mct_mod_oasis\g<end>'})
+
+
     def build(self, spec, prefix):
 
         with working_dir(self.build_directory):
             make('-f', self.makefile_file)
+
 
     def install(self, spec, prefix):
 
