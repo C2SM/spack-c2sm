467cf769d01d4169264251a76d328cce0697a05f
Annika <annika.lauber@c2sm.ethz.ch>
2023-03-30 16:51:06 +0200

    Fix path to log files (#706)
    - Fix path to log files
    - Rename log files only if None

--- test/integration_test.py
+++ test/integration_test.py
@@ -14,9 +14,10 @@ from src import machine_name, log_with_spack, sanitized_filename
 def spack_info(spec: str, log_filename: str = None):
     """
     Tests 'spack info' of the given spec and writes the output into the log file.
-    If log_filename is None, spec is used to create one.
     """
-    log_filename = sanitized_filename(f'{spec}-spack_info')
+
+    if log_filename is None:
+        log_filename = sanitized_filename(f'{spec}-spack_info')
     ret = log_with_spack(f'spack info {spec}', 'integration_test',
                          log_filename)
 
@@ -24,9 +25,10 @@ def spack_info(spec: str, log_filename: str = None):
 def spack_spec(spec: str, log_filename: str = None):
     """
     Tests 'spack info' of the given spec and writes the output into the log file.
-    If log_filename is None, spec is used to create one.
     """
-    log_filename = sanitized_filename(f'{spec}-spack_spec')
+
+    if log_filename is None:
+        log_filename = sanitized_filename(f'{spec}-spack_spec')
     ret = log_with_spack(f'spack spec {spec}', 'integration_test',
                          log_filename)
 
