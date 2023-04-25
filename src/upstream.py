f343c8b28c1071942b2b74f84a128be6d39ff7fd
Jonas Jucker <joni.jucker@yahoo.de>
2023-04-25 15:51:03 +0200

    module to remove upstreams

---
+++ src/upstream.py
@@ -0,0 +1,24 @@
+import shutil
+import sys
+import os
+from ruamel import yaml
+
+def read_upstream_from_spack_yaml(config_dir):
+    spack_yaml = os.path.join(config_dir,'spack.yaml')
+    y=yaml.YAML(typ='unsafe', pure=True)
+    spack_config = y.load(open(spack_yaml, 'r'))
+    upstream = spack_config['spack']['config']['install_tree']['root']
+    return upstream
+
+def delete_upstream(upstream):
+    print(f'Delete upstream {upstream}')
+    shutil.rmtree(upstream)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        raise ValueError('Need path to folder containing a spack.yaml')
+    else:
+        delete_upstream(read_upstream_from_spack_yaml(sys.argv[2]))
+
+
