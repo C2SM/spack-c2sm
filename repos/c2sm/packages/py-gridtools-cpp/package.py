2ba63d0217b21665165c11264a9e3827b2c35748
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-04-26 09:30:52 -0500

    ICON with DSL variants (#716)
    * add versions for icon4py and gt4py
    
    * GitHub Action: Apply Pep8-formatting
    
    * add fix for py-poetry-core
    
    * GitHub Action: Apply Pep8-formatting
    
    * updates to get icon4py building after the major gt4py overhaul
    
    * Update system_test.py
    
    * GitHub Action: Apply Pep8-formatting
    
    * add missing test deps, only test next and eve
    
    * remove obsolete patch
    
    * addressing some review comments
    
    * Add Boost to CMake Include Path
    
    * Change icon, gridtools and balfrin site packages for successful build
    
    * GitHub Action: Apply Pep8-formatting
    
    * set boost-path for unittests of icon4py, make tests on Balfrin work
    
    * lock python and gt4py versions
    
    * GitHub Action: Apply Pep8-formatting
    
    * fix typo
    
    * GitHub Action: Apply Pep8-formatting
    
    * Edits to support icon-dsl on Daint (#693)
    
    * working on daint
    
    * GitHub Action: Apply Pep8-formatting
    
    * tested for icon4py v0.0.2
    
    * GitHub Action: Apply Pep8-formatting
    
    * cleanup
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    * Reverting to clean versions of icon, nvidia blas and lapack
    
    * pulling changes from icon-gt4py-daint2 to support latest gt4py
    
    * addressing review comments
    
    * GitHub Action: Apply Pep8-formatting
    
    * icon with dsl variants
    
    * GitHub Action: Apply Pep8-formatting
    
    * bumpin icon4py to 0.0.3, gt4py tag uknown yet. Gt4py requirements now match the correct min requirements
    
    * numpy seems to depend on blas/lapack without this
    
    * remove artefact
    
    * addressing some review comments
    
    * GitHub Action: Apply Pep8-formatting
    
    * Update exclaim branch
    
    * trying new paths for icon4py
    
    * switching all gridtools references to py-gridtools-cpp and bumping to v2.3.0
    
    * GitHub Action: Apply Pep8-formatting
    
    * Changing to min requirements for py-gridtools-cpp
    
    * fix for cases when dsl=none
    
    * GitHub Action: Apply Pep8-formatting
    
    * Update repos/c2sm/packages/icon/package.py
    
    Co-authored-by: juckerj <39263956+jonasjucker@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jonas Jucker <joni.jucker@yahoo.de>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: juckerj <39263956+jonasjucker@users.noreply.github.com>
    Co-authored-by: Samuel Kellerhals <kellerhalssamuel@gmail.com>
    Co-authored-by: Christoph Müller <Christoph.Mueller@meteoswiss.ch>

--- repos/c2sm/packages/py-gridtools-cpp/package.py
+++ repos/c2sm/packages/py-gridtools-cpp/package.py
@@ -12,9 +12,15 @@ class PyGridtoolsCpp(PythonPackage):
     homepage = "https://gridtools.github.io/gridtools/latest/index.html"
 
     pypi = "gridtools-cpp/gridtools-cpp-2.2.3.tar.gz"
+    whl_url = "https://pypi.io/packages/py3/g/gridtools-cpp/gridtools_cpp-2.3.0-py3-none-any.whl"
 
     maintainers = ['samkellerhals']
 
+    version("2.3.0",
+            url=whl_url,
+            sha256=
+            "0af02845a538a7c20791ecaeeb3b68c8b976b653ef8593a00beea60445ec38b6",
+            expand=False)
     version("2.2.2",
             sha256=
             "d45316379440b6d96d04b3fb47f6a432946e9f9d906bcb10af51d9a92e95353e")
