a3f5cf097f069a69bc43d61c522cf364eebf6b10
Michael Jähn <mjaehn@ethz.ch>
2023-05-03 11:16:48 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,10 @@ templates_path = ['templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Copybutton prompt exceptions
+copybutton_prompt_text = r"\$ "
+copybutton_prompt_is_regexp = True
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
