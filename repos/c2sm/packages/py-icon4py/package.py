e1f6b8ed05c8f832d8affe0a49de479115ff53cd
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-05-16 14:50:16 +0200

    Update package.py

--- repos/c2sm/packages/py-icon4py/package.py
+++ repos/c2sm/packages/py-icon4py/package.py
@@ -20,7 +20,6 @@ class PyIcon4py(PythonPackage):
     maintainers = ['samkellerhals']
 
     version('main', branch='main', git=git)
-    version('test', tag='v0.0.3', git=git)
     version('0.0.3', tag='v0.0.3', git=git)
 
     depends_on('py-wheel', type='build')
