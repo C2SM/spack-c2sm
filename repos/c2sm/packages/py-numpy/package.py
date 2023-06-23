8d6a236cd64be0ee8a3b8a008c4235e17734966e
Dominic Hofer <dominic.hofer@meteoswiss.ch>
2023-06-23 14:50:23 +0200

    [numpy] Adds conflict.

--- repos/c2sm/packages/py-numpy/package.py
+++ repos/c2sm/packages/py-numpy/package.py
@@ -13,6 +13,8 @@ class PyNumpy(SpackPyNumpy):
             sha256=
             '003a9f530e880cb2cd177cba1af7220b9aa42def9c4afc2a2fc3ee6be7eb2b22')
 
+    conflicts("python@3.10.1", when="@1.24.2") # metadata generation fails.
+
     def url_for_version(self, version):
         url = "https://files.pythonhosted.org/packages/source/n/numpy/numpy-{}.{}"
         if version >= Version("1.23"):
