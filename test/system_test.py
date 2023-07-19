e112755463606195ef3f160766a773f7e6c164c2
juckerj <jonas.jucker@env.ethz.ch>
2023-07-19 08:52:21 +0200

    remove wrong test

--- test/system_test.py
+++ test/system_test.py
@@ -604,9 +604,6 @@ class PyHatchlingTest(unittest.TestCase):
 @pytest.mark.no_tsa  # py-isort install fails with: No module named 'poetry'.
 class PyIcon4pyTest(unittest.TestCase):
 
-    def test_install_0_0_4(self):
-        spack_install_and_test('py-icon4py @0.0.4')
-
     def test_install_version_0_0_3(self):
         spack_install_and_test(
             'py-icon4py @ 0.0.3 %gcc ^py-gt4py@1.1.1 ^python@3.10.4')
