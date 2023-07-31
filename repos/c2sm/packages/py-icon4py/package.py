41a260dfe06acbd624e7a07dc3c1cfbb3460dc80
Jonas Jucker <joni.jucker@yahoo.de>
2023-07-31 17:02:06 +0200

    fix testing for icon4py

--- repos/c2sm/packages/py-icon4py/package.py
+++ repos/c2sm/packages/py-icon4py/package.py
@@ -3,9 +3,13 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
 import os
 import inspect
+
+from spack import *
+from spack.build_systems.python import PythonPipBuilder
+
+import llnl.util.filesystem as fs
 
 
 class PyIcon4py(PythonPackage):
@@ -38,23 +42,42 @@ class PyIcon4py(PythonPackage):
     def setup_build_environment(self, env):
         env.set("CMAKE_INCLUDE_PATH", self.spec['boost'].prefix.include)
 
-    def install(self, spec, prefix):
+    def test(self):
+        # check if all installed module can be imported
+        super().test()
+
+        # unit tests
+        python('-m', 'pytest', '-v', '-s', '-n', 'auto', '--cov',
+               '--cov-append')
+
+
+class PythonPipBuilder(PythonPipBuilder):
+    def install(self, pkg, spec, prefix):
         """Install everything from build directory."""
 
-        args = PythonPackage._std_args(self) + ['--prefix=' + prefix]
+        args = PythonPipBuilder.std_args(pkg) + ["--prefix=" + prefix]
+
+        for key, value in self.config_settings(spec, prefix).items():
+            if spec["py-pip"].version < Version("22.1"):
+                raise SpecError(
+                    "'{}' package uses 'config_settings' which is only supported by "
+                    "pip 22.1+. Add the following line to the package to fix this:\n\n"
+                    '    depends_on("py-pip@22.1:", type="build")'.format(spec.name)
+                )
+
+            args.append("--config-settings={}={}".format(key, value))
 
         for option in self.install_options(spec, prefix):
-            args.append('--install-option=' + option)
+            args.append("--install-option=" + option)
         for option in self.global_options(spec, prefix):
-            args.append('--global-option=' + option)
+            args.append("--global-option=" + option)
 
-        if self.stage.archive_file and self.stage.archive_file.endswith(
-                '.whl'):
-            args.append(self.stage.archive_file)
+        if pkg.stage.archive_file and pkg.stage.archive_file.endswith(".whl"):
+            args.append(pkg.stage.archive_file)
         else:
-            args.append('.')
+            args.append(".")
 
-        pip = inspect.getmodule(self).pip
+        pip = inspect.getmodule(pkg).pip
 
         if self.spec.version == ver('0.0.3'):
             build_dirs = [
@@ -64,11 +87,5 @@ class PyIcon4py(PythonPackage):
             build_dirs = ['common', 'atm_dyn_iconam', 'tools']
 
         for dir in build_dirs:
-            with working_dir(os.path.join(self.stage.source_path, dir)):
+            with fs.working_dir(os.path.join(self.build_directory, dir)):
                 pip(*args)
-
-    @run_after('install')
-    @on_package_attributes(run_tests=True)
-    def install_test(self):
-        python('-m', 'pytest', '-v', '-s', '-n', 'auto', '--cov',
-               '--cov-append')
