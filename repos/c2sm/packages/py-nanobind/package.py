3cd4dc187883c3c4bf4987ae6de1eee7f7213e96
Abishek Gopal <abishek.gopal@c2sm.ethz.ch>
2023-08-24 18:19:14 +0200

    using builtin py-nanobind recipe for spack develop branch

--- repos/c2sm/packages/py-nanobind/package.py
+++ repos/c2sm/packages/py-nanobind/package.py
@@ -1,24 +1,36 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PyNanobind(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """nanobind -- Seamless operability between C++11 and Python.
 
-    homepage = "https://github.com/wjakob/nanobind"
-    url      = "https://files.pythonhosted.org/packages/cd/90/300cac4677ffdd95c5fac9cd1a64348370ce7f5f30e6c1042642ad907b1f/nanobind-1.5.1-py3-none-any.whl"
+    nanobind is a small binding library that exposes C++ types in
+    Python and vice versa. It is reminiscent of Boost.Python and pybind11
+    and uses near-identical syntax. In contrast to these existing tools,
+    nanobind is more efficient: bindings compile in a shorter amount of time,
+    produce smaller binaries, and have better runtime performance.
+    """
+
+    homepage = "https://nanobind.readthedocs.io"
+    url = "https://github.com/wjakob/nanobind/archive/refs/tags/v1.2.0.tar.gz"
+    git = "https://github.com/wjakob/nanobind.git"
 
     maintainers = ['abishekg7']
 
-    version('1.5.1', sha256='e4408ca6bcd424cb4555c6217cf7624d334862a6d497c549b01b9bc509e25b21', expand=False)
+    version("master", branch="master", submodules=True)
+    version("1.5.1", tag="v1.5.1", submodules=True)
+    version("1.4.0", tag="v1.4.0", submodules=True)
+    version("1.2.0", tag="v1.2.0", submodules=True)
 
+    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-scikit-build", type="build")
 
-    depends_on('py-scikit-build', type=('build', 'run'))
-    depends_on('ninja', type='build')
-    depends_on('py-setuptools@42:', type='build')
-    depends_on('cmake@3.17:', type='build')
-
+    depends_on("py-cmake@3.17:", type="build")
+    depends_on("py-ninja", type="build")
