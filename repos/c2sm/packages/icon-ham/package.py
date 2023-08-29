2dbfa3bcf56486a924f74184b9d7001dd7ed231d
stelliom <67868694+stelliom@users.noreply.github.com>
2023-08-29 15:53:46 +0200

    Removed references to ham variant to avoid redundancy with package name

--- repos/c2sm/packages/icon-ham/package.py
+++ repos/c2sm/packages/icon-ham/package.py
@@ -4,19 +4,15 @@ from spack.pkg.c2sm.icon import Icon as C2SMIcon
 
 class IconHam(C2SMIcon):
 
-    variant('ham', default=True, description='Enable the hammoz submodel')
-
     @run_before('build')
     def generate_hammoz_nml(self):
-        if '+ham' in self.spec:
-            with working_dir('./externals/atm_phy_echam_submodels/namelists'):
-                make()
+        with working_dir('./externals/atm_phy_echam_submodels/namelists'):
+            make()
 
     def configure_args(self):
         args = super().configure_args()
 
-        if '+ham' in self.spec:
-            args.append('--enable-atm-phy-echam-submodels')
-            args.append('--enable-hammoz')
+        args.append('--enable-atm-phy-echam-submodels')
+        args.append('--enable-hammoz')
 
         return args
