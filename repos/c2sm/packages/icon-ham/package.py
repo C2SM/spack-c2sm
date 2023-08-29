2b1ad6a9cd6509f29d03bb31f428279e3e4d861e
juckerj <39263956+jonasjucker@users.noreply.github.com>
2023-08-29 17:42:49 +0200

    [icon-ham] Removed references to +ham (#815)

--- repos/c2sm/packages/icon-ham/package.py
+++ repos/c2sm/packages/icon-ham/package.py
@@ -1,20 +1,18 @@
 from spack import *
-from spack.pkg.c2sm.icon import Icon
+from spack.pkg.c2sm.icon import Icon as C2SMIcon
 
 
-class IconHam(Icon):
+class IconHam(C2SMIcon):
 
-    @run_before('configure')
+    @run_before('build')
     def generate_hammoz_nml(self):
-        if '+ham' in self.spec:
-            with working_dir('./externals/atm_phy_echam_submodels/namelists'):
-                make()
+        with working_dir('./externals/atm_phy_echam_submodels/namelists'):
+            make()
 
     def configure_args(self):
-        args = Icon.configure_args(self)
+        args = super().configure_args()
 
-        if '+ham' in self.spec:
-            args.append('--enable-atm-phy-echam-submodels')
-            args.append('--enable-hammoz')
+        args.append('--enable-atm-phy-echam-submodels')
+        args.append('--enable-hammoz')
 
         return args
