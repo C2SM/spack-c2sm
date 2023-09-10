7bfce7233e498ef35253909bc78121c9a81ca1e4
Magdalena Luz <luzm@ethz.ch>
2023-09-10 22:07:54 +0200

    fix test function name.

--- test/integration_test.py
+++ test/integration_test.py
@@ -462,7 +462,7 @@ class SpecTest(unittest.TestCase):
     def test_py_ghex(self):
         spack_spec('py-ghex')
 
-    def test_hatch_vcs(self):
+    def test_py_hatch_vcs(self):
         spack_spec('py-hatch-vcs')
 
     def test_py_pyproject_metadata(self):
