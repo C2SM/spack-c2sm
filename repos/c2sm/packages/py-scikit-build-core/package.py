4efc14480cd762e5812ba2d4f3ec26ff21b11ab9
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-09-16 13:32:54 +0200

    removing the pypi arg from version()

--- repos/c2sm/packages/py-scikit-build-core/package.py
+++ repos/c2sm/packages/py-scikit-build-core/package.py
@@ -4,75 +4,12 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
-
-# TODO (magdalena) copy from https://github.com/spack/spack/blob/develop/var/spack/repos/builtin/packages/py-scikit-build-core/package.py
-#   add new version (0.5.0)
+from spack.pkg.builtin.py_scikit_build_core import PyScikitBuildCore as SpackPyScikitBuildCore
 
 
-class PyScikitBuildCore(PythonPackage):
+class PyScikitBuildCore(SpackPyScikitBuildCore):
     """scikit-build-core is a doubly improved build system generator
     for CPython C/C++/Fortran/Cython extensions. It features several
     improvements over the classic scikit-build build system generator."""
 
-    homepage = "https://github.com/scikit-build/scikit-build-core"
-    pypi = "scikit_build_core/scikit_build_core-0.2.0.tar.gz"
-    git = "https://github.com/scikit-build/scikit-build-core"
-
-    maintainers = ["wdconinc"]
-
-    version("0.2.0",
-            sha256=
-            "d2a76d9447a412038dc5e25dd259b03c25278661a0c7c3da766bb971c1a9acd2",
-            pypi=pypi)
-    version("0.5.0",
-            pypi="scikit_build_core/scikit_build_core-0.5.0.tar.gz",
-            sha256=
-            "a42a95029b34b5cf892855342d9b9445c774cb797fcb24c8fc4c2fb42b18dfca")
-
-    variant("pyproject",
-            default=False,
-            description="Enable pyproject.toml support")
-
-    depends_on("python@3.7:", type=("build", "run"))
-
-    # Build system
-    depends_on("py-hatchling", type="build")
-    depends_on("py-hatch-vcs", type="build")
-
-    # Dependencies
-    depends_on("py-exceptiongroup",
-               when="^python@:3.10",
-               type=("build", "run"))
-    depends_on("py-importlib-resources@1.3:",
-               when="^python@:3.8",
-               type=("build", "run"))
-    depends_on("py-packaging@20.9:", type=("build", "run"))
-    depends_on("py-tomli@1.1:", when="^python@:3.10", type=("build", "run"))
-    depends_on("py-typing-extensions@3.10:",
-               when="^python@:3.7",
-               type=("build", "run"))
-    depends_on("cmake@3.15:", type=("build", "run"))
-
-    # Optional dependencies
-    depends_on("py-pyproject-metadata@0.5:",
-               when="+pyproject",
-               type=("build", "run"))
-    depends_on("py-pathspec@0.10.1:", when="+pyproject", type=("build", "run"))
-
-    # Test dependencies
-    depends_on("py-build +virtualenv", type="test")
-    depends_on("py-cattrs@22.2:", type="test")
-    depends_on("py-importlib-metadata", when="^python@:3.7", type="test")
-    depends_on("py-pathspec@0.10.1:", type="test")
-    depends_on("py-pybind11", type="test")
-    depends_on("py-pyproject-metadata@0.5:", type="test")
-    depends_on("py-pytest@7:", type="test")
-    depends_on("py-pytest-subprocess@1.5:", type="test")
-    depends_on("py-setuptools", type="test")
-    depends_on("py-wheel", type="test")
-
-    @run_after("install")
-    @on_package_attributes(run_tests=True)
-    def install_test(self):
-        with working_dir("tests"):
-            which("pytest")()
+    version("0.5.0", sha256="a42a95029b34b5cf892855342d9b9445c774cb797fcb24c8fc4c2fb42b18dfca")
