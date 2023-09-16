4ad2a662fd642700bd983aff5d073801ba9041b0
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-09-16 23:27:25 +0200

    Update package.py

--- repos/c2sm/packages/serialbox/package.py
+++ repos/c2sm/packages/serialbox/package.py
@@ -8,4 +8,4 @@ class Serialbox(SpackSerialbox):
     git = 'https://github.com/GridTools/serialbox.git'
     maintainers = ['halungge', 'skosukhin']
 
-    version('2.6.1_2023-06-12', commit='88ac4e4dfc824953d068fe63c8e7b3dd9560a914', git=git, submodules=True)
+    version('2.6.2', commit='88ac4e4dfc824953d068fe63c8e7b3dd9560a914', git=git, submodules=True)
