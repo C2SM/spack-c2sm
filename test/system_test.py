13bb5d170664e32cd5f1e3520872d3bb9c151799
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-09-28 16:03:06 +0200

    Update system_test.py

--- test/system_test.py
+++ test/system_test.py
@@ -650,9 +650,14 @@ class PyGt4pyTest(unittest.TestCase):
 
     def test_install_version_1_1_2(self):
         spack_install_and_test('py-gt4py @1.1.2')
-
+      
+    @pytest.mark.no_daint  # fails with ModuleNotFoundError: No module named 'dace'
+    @pytest.mark.no_balfrin  # fails with ModuleNotFoundError: No module named 'dace'
     def test_install_version_1_1_3(self):
         spack_install_and_test('py-gt4py @1.1.3')
+
+    def test_install_version_1_1_4(self):
+        spack_install_and_test('py-gt4py @1.1.4')
 
 
 class PyHatchlingTest(unittest.TestCase):
@@ -676,7 +681,7 @@ class PyIcon4pyTest(unittest.TestCase):
 
     def test_install_version_0_0_8(self):
         spack_install_and_test(
-            'py-icon4py @ 0.0.8 %gcc ^py-gt4py@1.1.3 ^python@3.10.4')
+            'py-icon4py @ 0.0.8 %gcc ^py-gt4py@1.1.4 ^python@3.10.4')
 
 
 class PyInflectionTest(unittest.TestCase):
