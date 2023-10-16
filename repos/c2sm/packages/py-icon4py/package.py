4437e0c227aa256c806b72c3c8714953f770dc0e
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-10-16 15:39:13 +0200

    adding icon4py v0.0.8

--- repos/c2sm/packages/py-icon4py/package.py
+++ repos/c2sm/packages/py-icon4py/package.py
@@ -30,6 +30,7 @@ class PyIcon4py(PythonPackage):
     version('0.0.5', tag='v0.0.5', git=git)
     version('0.0.6', tag='v0.0.6', git=git)
     version('0.0.7', tag='v0.0.7', git=git)
+    version('0.0.8', tag='v0.0.8', git=git)
 
     depends_on('py-wheel', type='build')
     depends_on('py-setuptools', type='build')
