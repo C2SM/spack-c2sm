93981bc51c0db8e56c80fccb6bdda8c1347aa8a6
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2023-10-26 14:49:42 +0200

    add  fdb@5.11.23

--- repos/c2sm/packages/fdb/package.py
+++ repos/c2sm/packages/fdb/package.py
@@ -10,6 +10,9 @@ class Fdb(SpackFdb):
     """FDB (Fields DataBase) is a domain-specific object store developed at
     ECMWF for storing, indexing and retrieving GRIB data."""
 
+    version("5.11.23", 
+            sha256=
+            "09b1d93f2b71d70c7b69472dfbd45a7da0257211f5505b5fcaf55bfc28ca6c65")
     version("5.11.17",
             sha256=
             "375c6893c7c60f6fdd666d2abaccb2558667bd450100817c0e1072708ad5591e")
