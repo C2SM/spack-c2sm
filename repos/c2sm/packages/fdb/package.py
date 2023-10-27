3cba6d69c12bb85d81ef7172783f78219558fb0c
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2023-10-27 17:45:35 +0200

    test

--- repos/c2sm/packages/fdb/package.py
+++ repos/c2sm/packages/fdb/package.py
@@ -16,3 +16,25 @@ class Fdb(SpackFdb):
             "375c6893c7c60f6fdd666d2abaccb2558667bd450100817c0e1072708ad5591e")
 
     depends_on("ecbuild@3.7:", type="build", when="@5.11.6:")
+
+    def cmake_args(self):
+        enable_build_tools = "+tools" in self.spec
+
+        args = [
+            self.define("CTEST_OUTPUT_ON_FAILURE", 1),
+            self.define("ENABLE_FDB_BUILD_TOOLS", enable_build_tools),
+            self.define("ENABLE_BUILD_TOOLS", enable_build_tools),
+            # We cannot disable the FDB backend in indexed filesystem with
+            # table-of-contents because some default test programs and tools
+            # cannot be built without it:
+            self.define("ENABLE_TOCFDB", True),
+            self.define("ENABLE_LUSTRE", "backends=lustre" in self.spec),
+            self.define("ENABLE_PMEMFDB", False),
+            self.define("ENABLE_RADOSFDB", False),
+            # The tests download additional data (~10MB):
+            self.define("ENABLE_TESTS", self.run_tests),
+            # We do not need any experimental features:
+            self.define("ENABLE_EXPERIMENTAL", False),
+            self.define("ENABLE_SANDBOX", False),
+        ]
+        return args
