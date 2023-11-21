ea97e4aa5c821afa0304dbb71ced6d86ebfce9be
Victoria Cherkrkas <victoria.cherkas@meteoswiss.ch>
2023-11-21 16:08:58 +0100

    add libs to fdb-fortran

--- repos/c2sm/packages/fdb-fortran/package.py
+++ repos/c2sm/packages/fdb-fortran/package.py
@@ -17,12 +17,15 @@ class FdbFortran(CMakePackage):
 
     depends_on('fdb@5.11.0:')
 
+    @property
+    def libs(self):
+        return find_libraries("libfdbf", root=self.prefix, shared=False, recursive=True)
 
-def cmake_args(self):
-    args = [
-        self.define('Deckit_DIR', self.spec['eckit'].prefix),
-        self.define('Dmetkit_DIR', self.spec['metkit'].prefix),
-        self.define('Deccodes_DIR', self.spec['eccodes'].prefix),
-        self.define('Dfdb5_DIR', self.spec['fdb'].prefix),
-    ]
-    return args
+    def cmake_args(self):
+        args = [
+            self.define('Deckit_DIR', self.spec['eckit'].prefix),
+            self.define('Dmetkit_DIR', self.spec['metkit'].prefix),
+            self.define('Deccodes_DIR', self.spec['eccodes'].prefix),
+            self.define('Dfdb5_DIR', self.spec['fdb'].prefix),
+        ]
+        return args
