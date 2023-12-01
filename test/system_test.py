2e771a76580b3348cbbab2736686232aa0dcd2f2
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2023-12-01 17:23:28 +0100

    Update system_test.py

--- test/system_test.py
+++ test/system_test.py
@@ -634,7 +634,7 @@ class PyHatchlingTest(unittest.TestCase):
 @pytest.mark.no_tsa  # py-isort install fails with: No module named 'poetry'.
 class PyIcon4pyTest(unittest.TestCase):
 
-    def test_install_version_0_0_3(self):
+    def test_install_version_0_0_3_1(self):
         spack_install_and_test('py-icon4py @ 0.0.3.1 %gcc ^py-gt4py@1.0.1.1b')
 
     def test_install_version_0_0_9(self):
