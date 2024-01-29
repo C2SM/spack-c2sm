11740cef1af0ca487d5ca9e009946b115503978a
Michael Jähn <mjaehn@ethz.ch>
2024-01-29 15:03:54 +0100

    Add makedepf90 package for flexpart-cosmo

---
+++ repos/c2sm/packages/makedepf90/package.py
@@ -0,0 +1,47 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install makedepf
+#
+# You can edit this file again by typing:
+#
+#     spack edit makedepf
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack.package import *
+
+
+class Makedepf90(Package):
+    """ Makedepf90 is a program for automatic creation of
+        Makefile-style dependency lists for Fortran source code."""
+
+    homepage = "https://salsa.debian.org/science-team/makedepf90.git"
+    url = "https://salsa.debian.org/science-team/makedepf90.git"
+
+    maintainers("mjaehn")
+
+    version('3.0.1', git='https://salsa.debian.org/science-team/makedepf90.git', branch='debian/3.0.1-1')
+
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+
+    def install(self, spec, prefix):
+        # Run the configure script
+        configure = Executable('./configure')
+        configure('--prefix={0}'.format(prefix), '--bindir={0}'.format(prefix.bin))
+
+        # Build and install
+        make()
+        make('install')
