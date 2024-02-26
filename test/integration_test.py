586747c56a92b69c0b09bc31693eb34b2af3e417
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2024-02-26 10:49:23 +0100

    Revert "[fckit] Remove package and tests"
    This reverts commit fe41fc27b8e9d1297f3dc00521e317bdc1161dcd.

--- test/integration_test.py
+++ test/integration_test.py
@@ -67,6 +67,9 @@ class InfoTest(unittest.TestCase):
     def test_eccodes(self):
         spack_info('eccodes')
 
+    def test_fckit(self):
+        spack_info('fckit')
+
     def test_fdb_fortran(self):
         spack_info('fdb-fortran')
 
@@ -274,6 +277,9 @@ class SpecTest(unittest.TestCase):
 
     def test_eccodes(self):
         spack_spec('eccodes')
+
+    def test_fckit(self):
+        spack_spec('fckit')
 
     def test_fdb_fortran(self):
         spack_spec('fdb-fortran')
