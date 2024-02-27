95a033c72450bca6a36cdb54424aca3f01cec3aa
Victoria Cherkrkas <victoria.cherkas@meteoswiss.ch>
2024-02-27 16:09:43 +0100

    Fix comment

--- repos/c2sm/packages/fdb/package.py
+++ repos/c2sm/packages/fdb/package.py
@@ -5,6 +5,7 @@ from spack.pkg.builtin.fdb import Fdb as SpackFdb
 
 class Fdb(SpackFdb):
 
-    # This file can be removed when this PR https://github.com/spack/spack/pull/<INSERT> 
+    # This file can be removed when the following commit
+    # https://github.com/spack/spack/commit/8871bd5ba5c58562b8c20baa00f125aeccba586f
     # is included in a release on spack and this is used by spack-c2sm.
     depends_on("eckit@1.24.4:", when="@5.11.22:")
