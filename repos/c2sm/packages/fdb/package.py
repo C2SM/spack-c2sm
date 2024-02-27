299b88c459c17d0384f1173cf34bdf60345a5bd6
Victoria Cherkrkas <victoria.cherkas@meteoswiss.ch>
2024-02-27 17:06:33 +0100

    add find_libraries to fdb

--- repos/c2sm/packages/fdb/package.py
+++ repos/c2sm/packages/fdb/package.py
@@ -9,3 +9,10 @@ class Fdb(SpackFdb):
     # https://github.com/spack/spack/commit/8871bd5ba5c58562b8c20baa00f125aeccba586f
     # is included in a release on spack and this is used by spack-c2sm.
     depends_on("eckit@1.24.4:", when="@5.11.22:")
+
+    @property
+    def libs(self):
+        return find_libraries("libfdb5",
+                              root=self.prefix,
+                              shared=True,
+                              recursive=True)
