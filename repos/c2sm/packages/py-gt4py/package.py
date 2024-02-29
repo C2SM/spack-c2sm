a0f1119a49b50ab59a561d679bb0f1c13d11bc80
Edoardo Paone <edoardo.paone@cscs.ch>
2024-02-29 17:39:29 +0100

    Update gt4py version

--- repos/c2sm/packages/py-gt4py/package.py
+++ repos/c2sm/packages/py-gt4py/package.py
@@ -22,6 +22,7 @@ class PyGt4py(PythonPackage):
     version('1.0.3', tag='v1.0.3', git=url)
     version('1.0.3.1', tag='icon4py_20240214', git=url)
     version('1.0.3.2', tag='icon4py_20240226', git=url)
+    version('1.0.3.3', tag='icon4py_20240229', git=url)
 
     maintainers = ['samkellerhals']
 
