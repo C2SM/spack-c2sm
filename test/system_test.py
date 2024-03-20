209d3841137966512cbaf591d8db8974c86a087f
edopao <edoardo.paone@cscs.ch>
2024-03-20 13:46:24 +0100

    Update gt4py version (#925)

--- test/system_test.py
+++ test/system_test.py
@@ -643,6 +643,9 @@ class PyGt4pyTest(unittest.TestCase):
     def test_install_version_1_0_3_3(self):
         spack_install_and_test('py-gt4py @1.0.3.3')
 
+    def test_install_version_1_0_3_4(self):
+        spack_install_and_test('py-gt4py @1.0.3.4')
+
 
 class PyHatchlingTest(unittest.TestCase):
 
