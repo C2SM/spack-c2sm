8cc351811759717470ade63ca46dbc580b5908ca
Matthieu <matthieu.leclair@env.ethz.ch>
2024-04-15 14:37:47 +0200

    ref(uenv): rewrite pre-setup.sh

--- sysconfigs/uenv/pre-setup.sh
+++ sysconfigs/uenv/pre-setup.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -s /user-environment/config/compilers.yaml "$parent_dir"/sysconfigs/uenv/. 2> /dev/null
-ln -s /user-environment/config/upstreams.yaml "$parent_dir"/sysconfigs/uenv/. 2> /dev/null
-ln -s /user-environment/config/packages.yaml "$parent_dir"/sysconfigs/uenv/. 2> /dev/null
-ln -s /user-environment/repo "$parent_dir"/repos/uenv 2> /dev/null
+# TODO
+# Place holder to lock the current spack instance to a specific uenv
+# Maybe use a hash of the full recipe
+
+uenv_mount=/user-environment
+conf_dir="$parent_dir"/sysconfigs/uenv
+conf_files=(compilers.yaml upstreams.yaml packages.yaml)
+
+for conf_file in ${conf_files[@]}; do
+    [[ -f $conf_dir/$conf_file ]] || ln -s $uenv_mount/config/$conf_file $conf_dir
+done
+[[ -d "$parent_dir"/repos/uenv ]] || ln -s $uenv_mount/repo "$parent_dir"/repos/uenv
