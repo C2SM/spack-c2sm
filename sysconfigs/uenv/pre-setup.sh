c0d80c9147218849edcb71a469bfbd7dc1f4b92b
Matthieu <matthieu.leclair@env.ethz.ch>
2024-04-16 20:46:40 +0200

    add(uenv): lock spack-c2sm instance to a unique uenv

--- sysconfigs/uenv/pre-setup.sh
+++ sysconfigs/uenv/pre-setup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# TODO
-# Place holder to lock the current spack instance to a specific uenv
-# Maybe use a hash of the full recipe
-
 conf_dir="$parent_dir"/sysconfigs/uenv
 conf_files=(compilers.yaml upstreams.yaml packages.yaml)
 
+# lock the current spack instance to a specific uenv
+if [[ ! -f $uenv_mount/meta/hash ]]; then
+    echo "file $uenv_mount/meta/hash not found, spack-c2sm cannot be configured"
+    exit 1
+fi
+uenv_hash=$(cat $uenv_mount/meta/hash)
+if [[ -f "$conf_dir"/uenv.hash ]]; then
+    local_hash=$(cat "$conf_dir"/uenv.hash)
+    if [[ $uenv_hash != $local_hash ]]; then
+        echo "ERROR: This spack-c2sm instance was already once configured with a different uenv."
+        echo "       Please reuse the same uenv with the current instance or setup a fresh spack-c2sm"
+        echo "       with any uenv."
+        exit 1
+    fi
+else
+    echo $uenv_hash > $conf_dir/uenv.hash
+fi
+
+# Link configuration files to sysconfig
 for conf_file in ${conf_files[@]}; do
     [[ -f $conf_dir/$conf_file ]] || ln -s $uenv_mount/config/$conf_file $conf_dir
 done
