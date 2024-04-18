f9ea3dfc635de14deb199b8a55f42f0c2201cdc4
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2024-04-18 14:15:05 +0200

    Adding typing-extensions v4.10.0

--- repos/c2sm/packages/py-typing-extensions/package.py
+++ repos/c2sm/packages/py-typing-extensions/package.py
@@ -5,6 +5,9 @@ from spack.pkg.builtin.py_typing_extensions import PyTypingExtensions as SpackPy
 class PyTypingExtensions(SpackPyTypingExtensions):
     """Backported and Experimental Type Hints for Python 3.7+"""
 
+    version("4.10.0",
+            sha256=
+            "b0abd7c89e8fb96f98db18d86106ff1d90ab692004eb746cf6eda2682f91b3cb")
     version("4.5.0",
             sha256=
             "5cb5f4a79139d699607b3ef622a1dedafa84e115ab0024e0d9c044a9479ca7cb")
