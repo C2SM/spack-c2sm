4ea8fbf549e53217e9803cfd9abadfef8b988776
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-08-06 17:16:47 +0200

    Set $USER_ENV_ROOT on balfrin and tasan

--- setup-env.sh
+++ setup-env.sh
@@ -6,6 +6,10 @@ if [[ "$#" == 1 ]]; then
     machine="$1"
 else
     machine="$( "$parent_dir"/src/machine.sh )"
+    if [[ "$machine" == "balfrin" || "$machine" == "tasna" ]]; then
+        machine="$USER_ENV_ROOT"
+        exit 1
+    fi
 fi
 
 if [[ ${machine:0:1} == "/" ]]; then
