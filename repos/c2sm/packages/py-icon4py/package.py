bab0b3e5abd3eb9903c17170a416e25fc4c7bcdc
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-09-03 16:11:46 +0200

    added more things for tag

--- repos/c2sm/packages/py-icon4py/package.py
+++ repos/c2sm/packages/py-icon4py/package.py
@@ -27,6 +27,7 @@ class PyIcon4py(PythonPackage):
     version('0.0.10', tag='v0.0.10', git=git)
     version('0.0.11', tag='v0.0.11', git=git)
     version('0.0.12', tag='v0.0.12', git=git)
+    version('0.0.13', tag='v0.0.13', git=git)
 
     depends_on('py-wheel', type='build')
     depends_on('py-setuptools', type='build')
