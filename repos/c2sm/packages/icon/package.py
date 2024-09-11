96dccb3551f5e902ceedf3cd29fbd3b169068f8e
Dmitry Alexeev <53860485+alexeedm@users.noreply.github.com>
2024-09-11 10:39:25 +0200

    remove -lcudart in favor of -cuda

--- repos/c2sm/packages/icon/package.py
+++ repos/c2sm/packages/icon/package.py
@@ -541,7 +541,10 @@ class Icon(AutotoolsPackage, CudaPackage):
                 'NVCC={0}'.format(self.spec['cuda'].prefix.bin.nvcc)
             ])
 
-            libs += self.spec['cuda'].libs
+            # -cuda is an umbrella flag for NVHPC
+            # that pulls in all the needed CUDA libraries and wrappers
+            # It can't go into libs as spack prepends -l
+            config_vars['LDFLAGS'].extend(['-cuda'])
 
             cuda_host_compiler = self.compiler.cxx
             cuda_host_compiler_stdcxx_libs = self.compiler.stdcxx_libs
