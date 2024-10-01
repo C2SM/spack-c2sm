86539d6e3044fb50e45c608e645da027cb11da62
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-10-01 15:40:53 +0200

    Bring back Euler sysconfig

--- setup-env.sh
+++ setup-env.sh
@@ -6,6 +6,10 @@ if [[ "$#" == 1 ]]; then
     uenv="$1"
     export SPACK_UENV_PATH="$uenv"
     export SPACK_SYSTEM_CONFIG_PATH="$uenv"/config
+
+    if [[ uenv == "euler" ]]; then
+        export SPACK_SYSTEM_CONFIG_PATH="$parent_dir"/sysconfigs/euler
+    fi
 fi
 
 export SPACK_USER_CONFIG_PATH="$parent_dir"/user-config
@@ -13,7 +17,7 @@ export SPACK_USER_CACHE_PATH="$parent_dir"/user-cache
 . "$parent_dir"/spack/share/spack/setup-env.sh
 
 if [[ -n "$uenv" ]]; then
-    echo Spack configured with upstream "$uenv" .
+    echo Spack configured with upstream "$uenv".
 else
     echo Spack configured with no upstream.
 fi
