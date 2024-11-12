d99581c4fcd186da8abc532511dc98a49f95b1e5
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-11-12 11:23:00 +0100

    Fix icon-mch test

--- test/system_test.py
+++ test/system_test.py
@@ -42,7 +42,7 @@ def test_install_icon_mch_conditional_dependencies():
     # gpu=nvidia-80 triggers cuda
 
     spack_install(
-        'icon-mch @icon-2.6.6-mch2b %nvhpc +coupling serialization=create +emvorado +mpi gpu=nvidia-80 ^cray-mpich%nvhpc ^netcdf-fortran%nvhpc'
+        'icon-mch @icon-2.6.6-mch2b %nvhpc +coupling serialization=create +emvorado +mpi gpu=nvidia-80 ^cray-mpich%nvhpc ^netcdf-fortran%nvhpc ^eccodes%nvhpc'
     )
 
 
