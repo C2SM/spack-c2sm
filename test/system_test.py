dd468b2d7e3bf5813cb42f4aca783ee2f37ce1f0
juckerj <39263956+jonasjucker@users.noreply.github.com>
2025-02-11 08:50:21 +0100

    Update system_test.py

--- test/system_test.py
+++ test/system_test.py
@@ -52,7 +52,7 @@ def test_install_icon_conditional_dependencies():
 
 
 def test_install_icontools():
-    spack_install('icontools%gcc ~mpi ^netcdf-fortran%gcc')
+    spack_install('icontools%gcc ~mpi fflags="-fallow-argument-mismatch" ^netcdf-fortran%gcc')
 
 
 def test_install_libgrib1_nvhpc():
