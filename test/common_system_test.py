c6c062313423ed2699849bc65f9cd42508592bd1
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2025-03-06 11:11:59 +0100

    latest icon release

--- test/common_system_test.py
+++ test/common_system_test.py
@@ -29,7 +29,7 @@ def test_install_makedepf90():
 
 
 @pytest.mark.parametrize('version',
-                         ['2024.01-1', '2.6.6-mch2a', '2.6.6-mch2b'])
+                         ['2024.10', '2.6.6-mch2a', '2.6.6-mch2b'])
 def test_install_icon(version):
     # WORKAROUND: A build and link dependency should imply that the same compiler is used. ^cray-mpich%nvhpc enforces it.
     spack_install(f'icon @{version} %nvhpc ^cray-mpich%nvhpc')
