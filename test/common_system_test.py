d701310f5a3743f7cfa5ba042c2b48b7d807f0a8
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2025-03-12 08:34:57 +0100

    Enable CI for Santis using cscs-ci (#1047)
    * Create cscs.yml
    
    * Apply suggestions from code review
    
    fix typo
    
    * fix
    
    * remove dot
    
    * unit-test
    
    * integration test
    
    * cclone submodule
    
    * test_common
    
    * fix
    
    * sumarize logs
    
    * fix
    
    * archive artifacts
    
    * archive system tests
    
    * split system tests in two
    
    * GitHub Action: Apply Pep8-formatting
    
    * add dummy test
    
    * GitHub Action: Apply Pep8-formatting
    
    * use tokens for private repos
    
    * GitHub Action: Apply Pep8-formatting
    
    * test icon on santis
    
    * GitHub Action: Apply Pep8-formatting
    
    * revertme
    
    * reactivate token
    
    * debug clone
    
    * increase verbosity
    
    * pass git-config vars
    
    * GitHub Action: Apply Pep8-formatting
    
    * add config-count
    
    * drop env -i because git-config affected
    
    * GitHub Action: Apply Pep8-formatting
    
    * introduce stages
    
    * remove libgrib1
    
    * fix link for flexpart-cosmo
    
    * fix link for flexpart
    
    * remove duplicate code
    
    * latest icon release
    
    * add 2024.10 for icon
    
    * GitHub Action: Apply Pep8-formatting
    
    * remove failing tests
    
    * be more precise on testing
    
    * introduce SLURM_TIME_LIMIT and timeout
    
    * fix wrong formatting
    
    ---------
    
    Co-authored-by: Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
    Co-authored-by: github-actions <github-actions@github.com>

--- test/common_system_test.py
+++ test/common_system_test.py
@@ -22,33 +22,16 @@ def test_install_ecbuild():
     spack_install('ecbuild @3.7.2', test_root=False)
 
 
-def test_install_flexpart_cosmo():
-    spack_install('flexpart-cosmo')
+def test_install_makedepf90():
+    # Tests are disabled because they fail with:
+    # test1.sh: No such file or directory
+    spack_install('makedepf90', test_root=False)
 
 
-def test_install_flexpart_ifs():
-    spack_install('flexpart-ifs')
-
-
-@pytest.mark.parametrize('version',
-                         ['2024.01-1', '2.6.6-mch2a', '2.6.6-mch2b'])
+@pytest.mark.parametrize('version', ['2024.10', '2.6.6-mch2a', '2.6.6-mch2b'])
 def test_install_icon(version):
     # WORKAROUND: A build and link dependency should imply that the same compiler is used. ^cray-mpich%nvhpc enforces it.
     spack_install(f'icon @{version} %nvhpc ^cray-mpich%nvhpc')
-
-
-def test_install_icon_conditional_dependencies():
-    # +coupling triggers libfyaml, libxml2, netcdf-c
-    # serialization=create triggers serialbox
-    # +emvorado triggers eccodes, hdf5, zlib
-    # +eccodes-definitions triggers cosmo-eccodes-definitions
-    # +mpi triggers mpi
-    # gpu=nvidia-80 triggers cuda
-
-    # WORKAROUND: A build and link dependency should imply that the same compiler is used. ^cray-mpich%nvhpc enforces it.
-    spack_install(
-        'icon @2.6.6-mch2b %nvhpc +coupling serialization=create +emvorado +mpi gpu=nvidia-80 ^cray-mpich%nvhpc'
-    )
 
 
 # make check of external cdi fails with
@@ -56,16 +39,6 @@ def test_install_icon_conditional_dependencies():
 def test_install_icontools():
     spack_install('icontools @c2sm-master %gcc ~mpi ^netcdf-fortran%gcc',
                   test_root=False)
-
-
-def test_install_libgrib1_nvhpc():
-    spack_install('libgrib1 %nvhpc')
-
-
-def test_install_makedepf90():
-    # Tests are disabled because they fail with:
-    # test1.sh: No such file or directory
-    spack_install('makedepf90', test_root=False)
 
 
 def test_install_py_cytoolz():
@@ -82,26 +55,6 @@ def test_install_py_factory_boy():
 
 def test_install_py_gridtools_cpp():
     spack_install('py-gridtools-cpp')
-
-
-@pytest.mark.parametrize("version", ['1.0.3.9'])
-def test_install_py_gt4py_for_version(version):
-    spack_install(f'py-gt4py @{version}')
-
-
-# fails due to sql error
-def test_build_only_py_gt4py_for_1_0_3_10():
-    spack_install('py-gt4py @1.0.3.10', test_root=False)
-
-
-# fails due to sql error
-def test_build_only_py_icon4py_for_0_0_14():
-    spack_install('py-icon4py@ 0.0.14 ^py-gt4py @1.0.3.10', test_root=False)
-
-
-@pytest.mark.parametrize("version, gt4py_version", [('0.0.13', '1.0.3.9')])
-def test_install_py_icon4py(version, gt4py_version):
-    spack_install(f'py-icon4py@{version} ^py-gt4py@{gt4py_version}')
 
 
 def test_install_py_hatchling():
@@ -122,7 +75,3 @@ def test_install_py_tabulate():
 
 def test_install_py_typing_extensions():
     spack_install('py-typing-extensions')
-
-
-def test_install_yaxt():
-    spack_install('yaxt')
