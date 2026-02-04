976fd991b4e376e2d60e32aff847dec58a833ae2
Matthieu Leclair <mattthieu.leclair@c2sm.ethz.ch>
2026-02-04 12:20:31 +0100

    del: remmove all C2SM packages

--- repos/c2sm/packages/ecbuild/package.py
+++
@@ -1,9 +0,0 @@
-from spack.package import *
-from spack.pkg.builtin.ecbuild import Ecbuild as SpackEcbuild
-
-
-class Ecbuild(SpackEcbuild):
-
-    version('3.7.2',
-            sha256=
-            '7a2d192cef1e53dc5431a688b2e316251b017d25808190faed485903594a3fb9')
