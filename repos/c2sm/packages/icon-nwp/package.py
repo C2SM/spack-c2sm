9f9f14689897413b6a58be152a1de28c6a1c53dc
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2026-02-09 09:57:23 +0100

    rm header

--- repos/c2sm/packages/icon-nwp/package.py
+++ repos/c2sm/packages/icon-nwp/package.py
@@ -1,24 +1,3 @@
-# Copyright Spack Project Developers. See COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install icon-nwp
-#
-# You can edit this file again by typing:
-#
-#     spack edit icon-nwp
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 import os
 import os
 import re
