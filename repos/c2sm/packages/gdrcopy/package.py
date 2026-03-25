82560ec1a99e3d2a3f46052fdd63ac5ebfbcabe8
Mikael Simberg <mikael.simberg@iki.fi>
2026-03-25 13:52:34 +0100

    Add gdrcopy

---
+++ repos/c2sm/packages/gdrcopy/package.py
@@ -0,0 +1,57 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Gdrcopy(MakefilePackage, CudaPackage):
+    """A fast GPU memory copy library based on NVIDIA GPUDirect
+    RDMA technology."""
+
+    homepage = "https://github.com/NVIDIA/gdrcopy"
+    url = "https://github.com/NVIDIA/gdrcopy/archive/v2.1.tar.gz"
+    git = "https://github.com/NVIDIA/gdrcopy"
+    maintainers("scothalverson")
+
+    license("MIT")
+
+    version("master", branch="master")
+    version("2.5.1", sha256="c6d5ebb7dabb89d798f27609511735595004da73af28d93ac041bb5290c4cbec")
+    version("2.5", sha256="196400877be7e511edcf2a87b21a605cca99522ff217c97429348fd9153b30d7")
+    version("2.4.4", sha256="8802f7bc4a589a610118023bdcdd83c10a56dea399acf6eeaac32e8cc10739a8")
+    version("2.4.3", sha256="2727e671d6091f1178a1b10124c41f5a4dd5ce8a23b65a084ef00c178d5914b2")
+    version("2.4.2", sha256="ddea1986289e5cb3bb30185940f806d963f3d1e4839cc66cafc30f2388058c79")
+    version("2.4.1", sha256="faa7e816e9bad3301e53d6721457f7ef5ab42b7aa3b01ffda51f8e5620bb20ed")
+    version("2.3", sha256="b85d15901889aa42de6c4a9233792af40dd94543e82abe0439e544c87fd79475")
+    version("2.2", sha256="e4be119809391b18c735346d24b3b398dd9421cbff47ef12befbae40d61da45f")
+    version("2.1", sha256="cecc7dcc071107f77396f5553c9109790b6d2298ae29eb2dbbdd52b2a213e4ea")
+    version("2.0", sha256="98320e6e980a7134ebc4eedd6cf23647104f2b3c557f2eaf0d31a02609f5f2b0")
+    version("1.3", sha256="f11cdfe389b685f6636b80b4a3312dc014a385ad7220179c1318c60e2e28af3a")
+
+    # Don't call ldconfig: https://github.com/NVIDIA/gdrcopy/pull/229
+    patch("ldconfig.patch", when="@2.0:2.3")
+
+    depends_on("check")
+    requires("+cuda")
+
+    def setup_build_environment(self) -> None:
+        env.set("CUDA", self.spec["cuda"].prefix)
+
+        if self.spec.satisfies("@2.4:"):
+            env.set("NVCCFLAGS", " ".join(self.cuda_flags(self.spec.variants["cuda_arch"].values)))
+
+    def build(self, spec, prefix):
+        make("lib")
+        make("exes")
+
+    def install(self, spec, prefix):
+        mkdir(prefix.include)
+        mkdir(prefix.lib64)
+        if spec.satisfies("@2.2:"):
+            make("lib_install", "prefix={0}".format(self.prefix))
+            make("exes_install", "prefix={0}".format(self.prefix))
+        else:
+            make("lib_install", "PREFIX={0}".format(self.prefix))
+            make("exes_install", "PREFIX={0}".format(self.prefix))
