a2e1b6a5a1a4adf7fd76df6bb0225f59765388d4
Mikael Simberg <mikael.simberg@iki.fi>
2026-03-25 15:39:47 +0100

    Fix gdrcopy

--- repos/c2sm/packages/gdrcopy/package.py
+++ repos/c2sm/packages/gdrcopy/package.py
@@ -36,12 +36,6 @@ class Gdrcopy(MakefilePackage, CudaPackage):
     depends_on("check")
     requires("+cuda")
 
-    def setup_build_environment(self) -> None:
-        env.set("CUDA", self.spec["cuda"].prefix)
-
-        if self.spec.satisfies("@2.4:"):
-            env.set("NVCCFLAGS", " ".join(self.cuda_flags(self.spec.variants["cuda_arch"].values)))
-
     def build(self, spec, prefix):
         make("lib")
         make("exes")
