1d68977c03374c5522b41f1042fd0df28931351f
Mikael Stellio <67868694+stelliom@users.noreply.github.com>
2026-03-26 09:45:51 +0100

    Add action to check packages overlap between Spack-C2SM and Spack (#1075)

--- tools/package_overlap.py
+++ tools/package_overlap.py
@@ -1,7 +1,22 @@
 import os
+import sys
 
 spack = set(os.listdir("./spack/var/spack/repos/builtin/packages"))
 spack_c2sm = set(os.listdir("./repos/c2sm/packages"))
 
-for folder in sorted(spack & spack_c2sm):
-    print(folder)
+overlapping_packages = sorted(spack & spack_c2sm)
+
+# Allow duplicate icon package only before update to Spack v1.1
+overlapping_packages.remove("icon")
+
+if not overlapping_packages:
+    print("::notice::No overlapping packages!")
+else:
+    print("::error::Found overlapping packages between Spack-C2SM and Spack repos")
+
+    print("::group::Overlapping packages")
+    for package in overlapping_packages:
+        print(package)
+    print("::endgroup::")
+
+    sys.exit(1)
