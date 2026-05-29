b57ad3b89498b1a687f588d29b15662f547899a7
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2026-05-29 14:13:23 +0200

    Update to new repo format

--- tools/package_overlap.py
+++ tools/package_overlap.py
@@ -1,7 +1,7 @@
 import os
 
 spack = set(os.listdir("./spack/var/spack/repos/builtin/packages"))
-spack_c2sm = set(os.listdir("./repos/c2sm/packages"))
+spack_c2sm = set(os.listdir("./repos/spack_repo/c2sm/packages"))
 
 for folder in sorted(spack & spack_c2sm):
     print(folder)
