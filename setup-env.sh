0e912da487abc704cfa5baa6241c96270986618f
Matthieu Leclair <mattthieu.leclair@c2sm.ethz.ch>
2026-06-17 21:23:25 +0200

    ref[setup-env]: minor rewrite

--- setup-env.sh
+++ setup-env.sh
@@ -3,12 +3,12 @@
 parent_dir=$( cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" ; pwd -P )
 
 if [[ "$#" == 1 ]]; then
-    uenv="$1"
-    export SPACK_UENV_PATH="$uenv"
-    export SPACK_SYSTEM_CONFIG_PATH="$uenv"/config
-
-    if [[ $uenv == "euler" ]]; then
+    upstream="$1"
+    if [[ $upstream == "euler" ]]; then
         export SPACK_SYSTEM_CONFIG_PATH="$parent_dir"/sysconfigs/euler
+    else
+        export SPACK_UENV_PATH="$upstream"
+        export SPACK_SYSTEM_CONFIG_PATH="$upstream"/config
     fi
 fi
 
@@ -16,8 +16,8 @@ export SPACK_USER_CONFIG_PATH="$parent_dir"/user-config
 export SPACK_USER_CACHE_PATH="$parent_dir"/user-cache
 . "$parent_dir"/spack/share/spack/setup-env.sh
 
-if [[ -n "$uenv" ]]; then
-    echo Spack configured with upstream "$uenv".
+if [[ -n "$upstream" ]]; then
+    echo Spack configured with upstream "$upstream".
 else
     echo Spack configured with no upstream.
 fi
