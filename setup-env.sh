a054440ea78b98c07e9980fd2e7c67f6f4d3d3b5
Matthieu Leclair <mattthieu.leclair@c2sm.ethz.ch>
2026-06-29 17:23:19 +0200

    ref: split user-config for default and alps

--- setup-env.sh
+++ setup-env.sh
@@ -2,22 +2,26 @@
 
 parent_dir=$( cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" ; pwd -P )
 
+export SPACK_USER_CONFIG_PATH="${parent_dir}/user-config/default"
+
 if [[ "$#" == 1 ]]; then
     upstream="$1"
     if [[ $upstream == "euler" ]]; then
-        export SPACK_SYSTEM_CONFIG_PATH="$parent_dir"/sysconfigs/euler
+        export SPACK_SYSTEM_CONFIG_PATH="${parent_dir}/sysconfigs/euler"
+        export SPACK_USER_CONFIG_PATH="${parent_dir}/user-config/default"
     else
-        export SPACK_UENV_PATH="$upstream"
-        export SPACK_SYSTEM_CONFIG_PATH="$upstream"/config
+        # NOTE: SPACK_UENV_PATH used tests/spack_commands.py
+        export SPACK_UENV_PATH="${upstream}"
+        export SPACK_SYSTEM_CONFIG_PATH="${upstream}/config"
+        export SPACK_USER_CONFIG_PATH="${parent_dir}/user-config/alps"
     fi
 fi
 
-export SPACK_USER_CONFIG_PATH="$parent_dir"/user-config
-export SPACK_USER_CACHE_PATH="$parent_dir"/user-cache
-. "$parent_dir"/spack/share/spack/setup-env.sh
+export SPACK_USER_CACHE_PATH="${parent_dir}/user-cache"
+. "${parent_dir}/spack/share/spack/setup-env.sh"
 
 if [[ -n "$upstream" ]]; then
-    echo Spack configured with upstream "$upstream".
+    echo Spack configured with upstream "${upstream}".
 else
     echo Spack configured with no upstream.
 fi
