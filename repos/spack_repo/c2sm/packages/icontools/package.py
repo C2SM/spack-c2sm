120c9e443953930a9d8509cf805e149f7fb11aa7
Matthieu Leclair <mattthieu.leclair@c2sm.ethz.ch>
2026-06-30 14:57:33 +0200

    fix: no need to import those

--- repos/spack_repo/c2sm/packages/icontools/package.py
+++ repos/spack_repo/c2sm/packages/icontools/package.py
@@ -9,8 +9,6 @@ from collections import defaultdict
 
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack.package import *
-from spack.util.environment import is_system_path
-from llnl.util import tty
 
 
 class Icontools(AutotoolsPackage):
