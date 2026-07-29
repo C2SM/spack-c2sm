00f822503a909daf445af6586b4c9d401d9b51c0
Michael Jähn <mjaehn@ethz.ch>
2026-07-29 11:15:52 +0200

    Update setup-env.sh
    Co-authored-by: Mikael Stellio <67868694+stelliom@users.noreply.github.com>

--- setup-env.sh
+++ setup-env.sh
@@ -8,6 +8,7 @@ if [[ "$#" == 1 ]]; then
     upstream="$1"
     if [[ $upstream == "euler" ]]; then
         export SPACK_SYSTEM_CONFIG_PATH="/cluster/software/stacks/2026-06/config"
+        export SPACK_USER_CONFIG_PATH="${parent_dir}/user-config/default"
     else
         # NOTE: SPACK_UENV_PATH used tests/spack_commands.py
         export SPACK_UENV_PATH="${upstream}"
