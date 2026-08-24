e348c297779b0f030a70456731833f07df824fd9
Matthieu Leclair <mattthieu.leclair@c2sm.ethz.ch>
2026-08-24 14:31:03 +0200

    test: flags in fake upstream ICON recipe

--- repos/spack_repo/c2sm/packages/icon_nwp/package.py
+++ repos/spack_repo/c2sm/packages/icon_nwp/package.py
@@ -2,10 +2,11 @@ import os
 import re
 import glob
 from collections import defaultdict
-
 from itertools import chain
 
-from spack_repo.builtin.packages.icon.package import Icon
+from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
+
 from spack.package import *
 
 
@@ -29,6 +30,327 @@ def check_variant_extra_config_args(extra_config_arg):
         )
         return False
 
+
+class Icon(AutotoolsPackage):
+    """ICON - is a modeling framework for weather, climate, and environmental prediction. It solves
+    the full three-dimensional non-hydrostatic and compressible Navier-Stokes equations on an
+    icosahedral grid and allows seamless predictions from local to global scales."""
+
+    homepage = "https://www.icon-model.org"
+    url = "https://gitlab.dkrz.de/icon/icon-model/-/archive/icon-2024.01-public/icon-model-icon-2024.01-public.tar.gz"
+    git = "https://gitlab.dkrz.de/icon/icon-model.git"
+    submodules = True
+
+    maintainers("skosukhin", "Try2Code")
+
+    license("BSD-3-Clause", checked_by="skosukhin")
+
+    version(
+        "2025.10-2", tag="icon-2025.10-2-public", commit="69ef5cda9b81f5947e30d1016f41e7b37012f5de"
+    )
+    version(
+        "2025.04", tag="icon-2025.04-public", commit="1be2ca66ea0de149971d2e77e88a9f11c764bd22"
+    )
+    version("2024.10", sha256="5c461c783eb577c97accd632b18140c3da91c1853d836ca2385f376532e9bad1")
+    version("2024.07", sha256="f53043ba1b36b8c19d0d2617ab601c3b9138b90f8ff8ca6db0fd079665eb5efa")
+    version("2024.01-1", sha256="3e57608b7e1e3cf2f4cb318cfe2fdb39678bd53ca093955d99570bd6d7544184")
+    version("2024.01", sha256="d9408fdd6a9ebf5990298e9a09c826e8c15b1e79b45be228f7a5670a3091a613")
+
+    # Model Features:
+    variant("atmo", default=True, description="Enable the atmosphere component")
+    variant("les", default=True, description="Enable the Large-Eddy Simulation component")
+    variant("upatmo", default=True, description="Enable the upper atmosphere component")
+    variant("ocean", default=True, description="Enable the ocean component")
+    variant("jsbach", default=True, description="Enable the land component JSBACH")
+    variant("waves", default=True, description="Enable the ocean surface wave component")
+    variant("coupling", default=True, description="Enable the coupling")
+    variant("aes", default=True, description="Enable the AES physics package")
+    variant("nwp", default=True, description="Enable the NWP physics package")
+    variant(
+        "ecrad", default=False, description="Enable usage of the ECMWF radiation scheme (ECRAD)"
+    )
+    variant(
+        "rte-rrtmgp",
+        default=True,
+        description="Enable usage of the RTE+RRTMGP toolbox for radiation calculations",
+    )
+    variant(
+        "art", default=False, description="Enable the aerosols and reactive trace component ART"
+    )
+
+    # Infrastructural Features:
+    variant("mpi", default=True, description="Enable MPI (parallelization) support")
+    variant("openmp", default=False, description="Enable OpenMP support")
+
+    nvidia_targets = {"nvidia-{0}".format(cc): cc for cc in CudaPackage.cuda_arch_values}
+    # TODO: add AMD GPU support
+
+    variant(
+        "gpu",
+        default="none",
+        values=("none",) + tuple(nvidia_targets.keys()),
+        description="Enable GPU support for the specified architecture",
+    )
+    for __x in nvidia_targets.keys():
+        # Other compilers are not yet tested or supported, older NVHPC versions are not supported:
+        requires("%nvhpc@21.3:", when="gpu={0}".format(__x))
+
+    variant("mpi-gpu", default=True, description="Enable usage of the GPU-aware MPI features")
+    requires("+mpi", when="+mpi-gpu")
+    conflicts("gpu=none", when="+mpi-gpu")
+
+    variant("grib2", default=False, description="Enable GRIB2 I/O")
+
+    variant(
+        "parallel-netcdf",
+        default=False,
+        description="Enable usage of the parallel features of NetCDF",
+    )
+    requires("+mpi", when="+parallel-netcdf")
+
+    variant("cdi-pio", default=False, description="Enable usage of the parallel features of CDI")
+    requires("+mpi", when="+cdi-pio")
+
+    variant("yaxt", default=False, description="Enable the YAXT data exchange")
+    requires("+mpi", when="+yaxt")
+
+    serialization_values = ("read", "perturb", "create")
+    variant(
+        "serialization",
+        default="none",
+        values=("none",) + serialization_values,
+        description="Enable the Serialbox2 serialization",
+    )
+
+    variant("comin", default=False, description="Enable the ICON community interfaces")
+
+    # Optimization Features:
+    variant("mixed-precision", default=False, description="Enable mixed-precision dynamical core")
+
+    variant("single-precision", default=False, description="Enable single-precision")
+    conflicts(
+        "+single-precision",
+        when="@:2025.03",
+        msg="+single-precision requires icon version 2025.04 or newer",
+    )
+
+    variant(
+        "single-precision-ecrad", default=False, description="Enable single-precision for ecRad"
+    )
+    conflicts(
+        "+single-precision-ecrad",
+        when="@:2025.09",
+        msg="+single-precision-ecrad requires icon version 2025.10 or newer",
+    )
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build")
+    depends_on("python", type="build")
+    depends_on("perl", type="build")
+    depends_on("cmake@3.18:", type="build")
+    depends_on("gmake@3.81:", type="build")
+    depends_on("findutils", type="build")
+
+    depends_on("libxml2", when="+art")
+    depends_on("libfyaml@0.6:", when="+coupling")
+    for __x in serialization_values:
+        depends_on("serialbox+fortran", when="serialization={0}".format(__x))
+    depends_on("eccodes", when="+grib2")
+    depends_on("lapack")
+    depends_on("blas")
+    depends_on("netcdf-fortran")
+    depends_on("netcdf-c")
+    depends_on("netcdf-c+mpi", when="+parallel-netcdf")
+    depends_on("mpi", when="+mpi")
+
+    for __x in nvidia_targets.keys():
+        depends_on("cuda", when="gpu={0}".format(__x))
+
+    def __init__(self, spec: spack.spec.Spec) -> None:
+        super().__init__(spec)
+        self.single_args: list[str] = []
+        self.flags: dict(str, list[str]) = defaultdict(list)
+
+    def set_configure_args(self) -> None:
+        self.single_args.append("--disable-rpaths")
+        libs = LibraryList([])
+
+        for x in [
+            "atmo",
+            "les",
+            "upatmo",
+            "ocean",
+            "jsbach",
+            "waves",
+            "aes",
+            "nwp",
+            "ecrad",
+            "rte-rrtmgp",
+            "openmp",
+            "mpi-gpu",
+            "parallel-netcdf",
+            "cdi-pio",
+            "yaxt",
+            "mixed-precision",
+            "single-precision",
+            "single-precision-ecrad",
+            "comin",
+        ]:
+            self.single_args.extend(self.enable_or_disable(x))
+
+        if self.spec.satisfies("+art"):
+            self.single_args.append("--enable-art")
+            libs += self.spec["libxml2"].libs
+        else:
+            self.single_args.append("--disable-art")
+
+        if self.spec.satisfies("+coupling"):
+            self.single_args.append("--enable-coupling")
+            libs += self.spec["libfyaml"].libs
+        else:
+            self.single_args.append("--disable-coupling")
+
+        serialization = self.spec.variants["serialization"].value
+        if serialization == "none":
+            self.single_args.append("--disable-serialization")
+        else:
+            self.single_args.extend(
+                [
+                    "--enable-serialization={0}".format(serialization),
+                    "SB2PP={0}".format(self.spec["serialbox"].pp_ser),
+                ]
+            )
+            libs += self.spec["serialbox:fortran"].libs
+
+        if self.spec.satisfies("+grib2"):
+            self.single_args.append("--enable-grib2")
+            libs += self.spec["eccodes:c"].libs
+        else:
+            self.single_args.append("--disable-grib2")
+
+        libs += self.spec["lapack:fortran"].libs
+        libs += self.spec["blas:fortran"].libs
+        libs += self.spec["netcdf-fortran"].libs
+        libs += self.spec["netcdf-c"].libs
+
+        if self.spec.satisfies("+mpi"):
+            self.single_args.extend(
+                [
+                    "--enable-mpi",
+                    # We cannot provide a universal value for MPI_LAUNCH, therefore we have to
+                    # disable the MPI checks:
+                    "--disable-mpi-checks",
+                    "CC=" + self.spec["mpi"].mpicc,
+                    "FC=" + self.spec["mpi"].mpifc,
+                ]
+            )
+        else:
+            self.single_args.append("--disable-mpi")
+
+        gpu = self.spec.variants["gpu"].value
+
+        if gpu in self.nvidia_targets:
+            self.single_args.append("--enable-gpu=openacc+cuda")
+            self.flags["CUDAFLAGS"] = [
+                "-g",
+                "-O3",
+                "-arch=sm_{0}".format(self.nvidia_targets[gpu]),
+                "-ccbin={0}".format(spack_cxx),
+            ]
+            libs += self.spec["cuda"].libs
+        else:
+            self.single_args.append("--disable-gpu")
+
+        if gpu in self.nvidia_targets or self.spec.satisfies("+comin"):
+            self.flags["ICON_LDFLAGS"].extend(self.compiler.stdcxx_libs)
+
+        if self.compiler.name == "gcc":
+            self.flags["CFLAGS"].append("-g")
+            self.flags["ICON_CFLAGS"].append("-O3")
+            self.flags["ICON_BUNDLED_CFLAGS"].append("-O2")
+            self.flags["FCFLAGS"].append("-g")
+            self.flags["ICON_FCFLAGS"].append("-O2")
+            if self.spec.satisfies("+ocean"):
+                self.flags["ICON_OCEAN_FCFLAGS"].extend(["-O3", "-fno-tree-loop-vectorize"])
+                self.single_args.extend(
+                    ["--enable-fcgroup-OCEAN", "ICON_OCEAN_PATH=src/hamocc:src/ocean:src/sea_ice"]
+                )
+
+        elif self.compiler.name in ["intel", "oneapi"]:
+            self.single_args.append("--enable-intel-consistency")
+
+            self.flags["CFLAGS"].extend(["-g", "-ftz", "-fma", "-ip", "-qno-opt-dynamic-align"])
+            self.flags["ICON_CFLAGS"].append("-O3")
+            self.flags["ICON_BUNDLED_CFLAGS"].append("-O2")
+            self.flags["FCFLAGS"].extend(["-g", "-fp-model source"])
+            self.flags["ICON_FCFLAGS"].extend(
+                [
+                    "-O3",
+                    "-ftz",
+                    "-qoverride-limits",
+                    "-assume realloc_lhs",
+                    "-align array64byte",
+                    "-fma",
+                    "-ip",
+                ]
+            )
+
+            if self.spec.satisfies("+coupling%oneapi"):
+                self.flags["ICON_YAC_CFLAGS"].extend(["-O2", "-fp-model precise"])
+
+            if self.spec.satisfies("+ocean"):
+                self.flags["ICON_OCEAN_FCFLAGS"].extend(
+                    ["-O3", "-assume norealloc_lhs", "-reentrancy threaded"]
+                )
+                self.single_args.extend(
+                    ["--enable-fcgroup-OCEAN", "ICON_OCEAN_PATH=src/hamocc:src/ocean:src/sea_ice"]
+                )
+
+                if self.spec.satisfies("+openmp"):
+                    self.flags["ICON_OCEAN_FCFLAGS"].extend(["-DOCE_SOLVE_OMP"])
+
+            if self.spec.satisfies("+ecrad"):
+                self.flags["ICON_ECRAD_FCFLAGS"].extend(["-qno-opt-dynamic-align", "-no-fma", "-fpe0"])
+
+        elif self.compiler.name == "nvhpc":
+            self.flags["CFLAGS"].extend(["-g", "-O2"])
+            self.flags["FCFLAGS"].extend(
+                ["-g", "-O2", "-Mrecursive", "-Mallocatable=03", "-Mstack_arrays"]
+            )
+
+            if gpu in self.nvidia_targets:
+                self.flags["FCFLAGS"].extend(
+                    ["-acc=gpu", "-gpu=cc{0}".format(self.nvidia_targets[gpu])]
+                )
+
+            if self.spec.satisfies("+coupling%nvhpc@:23.9"):
+                self.single_args.append("yac_cv_fc_is_contiguous_works=yes")
+
+        else:
+            self.flags["CFLAGS"].extend(["-g", "-O2"])
+            self.flags["FCFLAGS"].extend(["-g", "-O2"])
+
+        self.flags["LIBS"].append(libs.link_flags)
+
+    def configure_args(self) -> list[str]:
+        # Populate self.single_args and self.flags
+        self.set_configure_args()
+        # Remove duplicates while keeping the original order
+        self.single_args = list(dict.fromkeys(self.single_args))
+        for key, values in self.flags.items():
+            self.flags[key] = list(dict.fromkeys(values))
+        # Return final list
+        return [
+            *chain(
+                self.single_args,
+                (
+                    "{0}={1}".format(name, " ".join(values))
+                    for name, values in self.flags.items()
+                ),
+            )
+        ]
+    
 
 class IconNwp(Icon):
     """ICON - is a modeling framework for weather, climate, and environmental
@@ -187,40 +509,6 @@ class IconNwp(Icon):
     # patches
     patch("mo_nh_stepping_null_pointer.patch", when="%fortran=nvhpc@26.1")
 
-    FLAG_KEYS = [
-        "LIBS",
-        "CFLAGS",
-        "FCFLAGS",
-        "ICON_FCFLAGS",
-        "LDFLAGS",
-        "ICON_LDFLAGS",
-        "ICON_BUNDLED_CFLAGS",
-        "ICON_YAC_CFLAGS",
-        "ICON_OCEAN_FCFLAGS",
-        "ICON_ECRAD_FCFLAGS",
-        "CUDAFLAGS",
-    ]
-
-    # NOTE: The aim is to upstream `set_configure_args` to `spack_repo.builtin.packages.icon.package`.
-    #       This way, we only need to directly modify `set_configure_args` like this:
-    #       ````
-    #       def set_configure_args(self) -> None:
-    #           super().set_configure_args()
-    #           # modify self.single_args and self.flags
-    #       ````
-    #       The `parse_config_args` method would also disappear then.
-
-    def parse_config_args(self, args) -> None:
-        self.single_args = []
-        self.flags = defaultdict(list)
-        for a in args:
-            for key in self.FLAG_KEYS:
-                if a.startswith(f"{key}="):
-                    self.flags[key].append(a.split("=", 1)[1].strip())
-                    break
-            else:
-                self.single_args.append(a)
-
     # TODO: install icon4py from within the icon package recipe
     #       following a similar strategy as the icon4py package.
     #       Also make sure to point to the uv found as dependency
@@ -237,7 +525,7 @@ class IconNwp(Icon):
             env.prepend_path("PATH", icon4py_bin_dir)
 
     def set_configure_args(self) -> None:
-        self.parse_config_args(super().configure_args())
+        super().set_configure_args()
         libs = LibraryList([])
 
         # The base class hardcodes FCFLAGS per compiler vendor and never
@@ -318,24 +606,6 @@ class IconNwp(Icon):
             self.flags["LDFLAGS"].extend(non_system_reversed_lib_dirs)
 
         self.flags["LIBS"].append(libs.link_flags)
-
-    def configure_args(self):
-        # Set configure args
-        self.set_configure_args()
-        # Remove duplicates
-        self.single_args = list(set(self.single_args))
-        for key, values in self.flags.items():
-            self.flags[key] = list(set(values))
-        # Return final list
-        return [
-            *chain(
-                self.single_args,
-                (
-                    "{0}={1}".format(name, " ".join(values))
-                    for name, values in self.flags.items()
-                ),
-            )
-        ]
 
     def strip_variant_prefix(self, variant_string):
         prefixes = ["--enable-", "--disable-"]
